70e122fa9d649130c2fa9d1aba944d78d683f0ff
Oleksandr Leontiev <oleo@softserveinc.com>
2022-04-05 14:39:10 +0000

    Merged PR 7631: Components prepare
    Added several syncfusion packages for components and bare material theme
    Added brand favicon and meta descriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SidebarComponent } from '@syncfusion/ej2-angular-navigations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'einstein-ui';
+  @ViewChild('sidebar') sidebar: SidebarComponent;
+
+  public open() {
+    console.log('Sidebar Opened');
+  }
+
+  public close() {
+    console.log('Sidebar Closed');
+  }
+
+  toggleClick() {
+    this.sidebar.toggle();
+  }
+
+  closeClick() {
+    this.sidebar.hide();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SidebarModule } from '@syncfusion/ej2-angular-navigations';
+import { PagerModule } from '@syncfusion/ej2-angular-grids';
+import { RadioButtonModule } from '@syncfusion/ej2-angular-buttons';
+import { TextBoxModule } from '@syncfusion/ej2-angular-inputs';
+import { enableRipple } from '@syncfusion/ej2-base';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+enableRipple(true);
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    SidebarModule,
+    PagerModule,
+    RadioButtonModule,
+    TextBoxModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
