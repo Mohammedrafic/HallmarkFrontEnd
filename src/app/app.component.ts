6d02ce654d5bdc5b27eadd8398e69655244160f7
Dmytro Yermolaiev <dyermol@softserveinc.com>
2022-04-06 10:32:21 +0000

    Merged PR 7668: added theming mechanism
    added theming mechanism

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component, ViewChild } from '@angular/core';
 import { SidebarComponent } from '@syncfusion/ej2-angular-navigations';
 
+enum THEME {
+  light = 'light',
+  dark = 'dark'
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  private darkTheme = true;
+
   @ViewChild('sidebar') sidebar: SidebarComponent;
 
   public open() {
@@ -23,5 +30,15 @@ export class AppComponent {
 
   closeClick() {
     this.sidebar.hide();
+  }
+
+  toggleTheme() {
+    this.darkTheme = !this.darkTheme;
+    this.setTheme(this.darkTheme);
+  }
+
+  private setTheme(darkTheme: boolean) {
+    document.body.classList.remove(darkTheme ? THEME.light : THEME.dark);
+    document.body.classList.add(darkTheme ? THEME.dark : THEME.light);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Registering Syncfusion license key
+registerLicense('ORg4AjUWIQA/Gnt2VVhhQlFaclhJWHxMYVF2R2FJelRwd19CZkwgOX1dQl9hSXpTc0VmWXpddnxWQWA='); //Trial key
+
 if (environment.production) {
   enableProdMode();
 }
