4c2847ccd443f6889a52a9149d591b4002d28c97
Dmytro Yermolayev <dyermol@softserveinc.com>
2022-04-08 10:39:23 +0300

    removed headers components

--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -1,80 +1,46 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { DashboardHeaderComponent } from './dashboard/dashboard-header/dashboard-header.component';
 import { DashboardContentComponent } from './dashboard/dashboard-content/dashboard-content.component';
-import { ClientManagementHeaderComponent } from './client-management/client-management-header/client-management-header.component';
 import { ClientManagementContentComponent } from './client-management/client-management-content/client-management-content.component';
 import { TimesheetsContentComponent } from './timesheets/timesheets-content/timesheets-content.component';
-import { TimesheetsHeaderComponent } from './timesheets/timesheets-header/timesheets-header.component';
-import { InvoicesHeaderComponent } from './invoices/invoices-header/invoices-header.component';
 import { InvoicesContentComponent } from './invoices/invoices-content/invoices-content.component';
-import { CandidatesHeaderComponent } from './candidates/candidates-header/candidates-header.component';
 import { CandidatesContentComponent } from './candidates/candidates-content/candidates-content.component';
-import { ReportsHeaderComponent } from './reports/reports-header/reports-header.component';
 import { ReportsContentComponent } from './reports/reports-content/reports-content.component';
+import { ClientPageComponent } from './client-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardHeaderComponent,
-    outlet: 'header',
+    component: ClientPageComponent,
+    children: [
+      {
+        path: '',
+        component: DashboardContentComponent,
+      },
+      {
+        path: 'client-management',
+        component: ClientManagementContentComponent,
+      },
+      {
+        path: 'time-sheets',
+        component: TimesheetsContentComponent,
+      },
+      {
+        path: 'invoices',
+        component: InvoicesContentComponent,
+      },
+      {
+        path: 'candidates',
+        component: CandidatesContentComponent,
+      },
+      {
+        path: 'reports',
+        component: ReportsContentComponent,
+      },
+    ]
   },
-  {
-    path: '',
-    component: DashboardContentComponent,
-    outlet: 'content',
-  },
-  {
-    path: 'client-management',
-    component: ClientManagementHeaderComponent,
-    outlet: 'header',
-  },
-  {
-    path: 'client-management',
-    component: ClientManagementContentComponent,
-    outlet: 'content',
-  },
-  {
-    path: 'time-sheets',
-    component: TimesheetsHeaderComponent,
-    outlet: '',
-  },
-  {
-    path: 'time-sheets',
-    component: TimesheetsContentComponent,
-    outlet: 'content',
-  },
-  {
-    path: 'invoices',
-    component: InvoicesHeaderComponent,
-    outlet: 'header',
-  },
-  {
-    path: 'invoices',
-    component: InvoicesContentComponent,
-    outlet: 'content',
-  },
-  {
-    path: 'candidates',
-    component: CandidatesHeaderComponent,
-    outlet: 'header',
-  },
-  {
-    path: 'candidates',
-    component: CandidatesContentComponent,
-    outlet: 'content',
-  },
-  {
-    path: 'reports',
-    component: ReportsHeaderComponent,
-    outlet: 'header',
-  },
-  {
-    path: 'reports',
-    component: ReportsContentComponent,
-    outlet: 'content',
-  },
+  
 ];
 
 @NgModule({
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -2,33 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientRoutingModule } from './client-routing.module';
-import { DashboardHeaderComponent } from './dashboard/dashboard-header/dashboard-header.component';
 import { DashboardContentComponent } from './dashboard/dashboard-content/dashboard-content.component';
 import { ClientManagementContentComponent } from './client-management/client-management-content/client-management-content.component';
-import { ClientManagementHeaderComponent } from './client-management/client-management-header/client-management-header.component';
-import { CandidatesHeaderComponent } from './candidates/candidates-header/candidates-header.component';
 import { CandidatesContentComponent } from './candidates/candidates-content/candidates-content.component';
 import { InvoicesContentComponent } from './invoices/invoices-content/invoices-content.component';
-import { InvoicesHeaderComponent } from './invoices/invoices-header/invoices-header.component';
-import { ReportsHeaderComponent } from './reports/reports-header/reports-header.component';
-import { TimesheetsHeaderComponent } from './timesheets/timesheets-header/timesheets-header.component';
 import { TimesheetsContentComponent } from './timesheets/timesheets-content/timesheets-content.component';
 import { ReportsContentComponent } from './reports/reports-content/reports-content.component';
-import { ClientPageComponent } from './client-page/client-page.component';
+import { ClientPageComponent } from './client-page.component';
 
 @NgModule({
   declarations: [
-    DashboardHeaderComponent,
     DashboardContentComponent,
     ClientPageComponent,
     ClientManagementContentComponent,
-    ClientManagementHeaderComponent,
-    CandidatesHeaderComponent,
     CandidatesContentComponent,
     InvoicesContentComponent,
-    InvoicesHeaderComponent,
-    ReportsHeaderComponent,
-    TimesheetsHeaderComponent,
     TimesheetsContentComponent,
     ReportsContentComponent,
   ],
