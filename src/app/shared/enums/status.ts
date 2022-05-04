b098f862f28ef003fdbbeac59e5aac65c67dd8b7
Oleksandr Leontiev <oleo@softserveinc.com>
2022-05-04 16:01:43 +0000

    Merged PR 9234: EIN-444 Modify styles for grid row action buttons
    * Added classes for icons
    * Modified layout for grid row tool buttons

--- src/app/shared/enums/status.ts
+++ src/app/shared/enums/status.ts
@@ -1,12 +1,12 @@
 export enum Status {
-  'Work In Progress',
+  'In Progress',
   'Inactive',
   'Active',
   'Suspended'
 }
 
 export const STATUS_COLOR_GROUP = {
-  'e-success': ['open', 'Active', 'Work In Progress'],
+  'e-success': ['open', 'Active', 'In Progress'],
   'e-warning': ['incomplete', 'Suspended'],
   'e-default': ['closed', 'Inactive'],
   'e-pending': ['pending'],
