75e576651983f0b1c7f10f3f1e7eb4bf0ba264ae
Kostiantyn Shyika <kshyi@softserveinc.com>
2022-05-19 08:10:45 +0000

    Merged PR 10364: EIN-579/581 Implemented organization/agency switcher
    EIN-579 and EIN-581 Implemented organization/agency switcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+
+import { Store } from '@ngxs/store';
+
+import { ORGANIZATION_URL_AREAS_REG_EXPS, AGENCY_URL_AREAS_REG_EXPS } from '@shared/constants';
+import { SetIsOrganizationAgencyArea } from './store/app.actions';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +12,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private router: Router, private store: Store) {
+    this.router.events.pipe().subscribe((event: any) => {
+      if (event instanceof NavigationStart) {
+        const isOrganizationArea = ORGANIZATION_URL_AREAS_REG_EXPS.some(regexp => regexp.test(event.url));
+        const isAgencyArea = AGENCY_URL_AREAS_REG_EXPS.some(regexp => regexp.test(event.url));
+
+        this.store.dispatch(new SetIsOrganizationAgencyArea({ isOrganizationArea, isAgencyArea }));
+      }
+    });
+  }
 }
