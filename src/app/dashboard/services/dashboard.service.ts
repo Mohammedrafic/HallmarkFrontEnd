b62142343ceff0c26c7418157014485e738a30a5
Roman Bishko <bishko86@yahoo.com>
2022-05-30 13:48:14 +0300

    #965 base dashboard layout according to Figma

--- src/app/dashboard/services/dashboard.service.ts
+++ src/app/dashboard/services/dashboard.service.ts
@@ -10,17 +10,20 @@ export class DashboardService {
   getDashboardsPanels(): Observable<PanelModel[]> {
     const dashboardData = localStorage.getItem('dashboard');
     if (dashboardData) {
-      const panels = JSON.parse(dashboardData)
+      const panels = JSON.parse(dashboardData);
       return of(panels);
     }
     return of([
-      { id: 'layout_0', sizeX: 1, sizeY: 1, row: 0, col: 0, content: '<div class="content">1</div>' },
-      { id: 'layout_1', sizeX: 1, sizeY: 1, row: 0, col: 1, content: '<div class="content">2</div>' },
-      { id: 'layout_2', sizeX: 1, sizeY: 1, row: 0, col: 2, content: '<div class="content">3</div>' },
-      { id: 'layout_3', sizeX: 1, sizeY: 1, row: 0, col: 3, content: '<div class="content">4</div>' },
-      { id: 'layout_4', sizeX: 1, sizeY: 1, row: 0, col: 4, content: '<div class="content">5</div>' },
-      { id: 'layout_5', sizeX: 1, sizeY: 1, row: 1, col: 0, content: '<div class="content">6</div>' },
-      { id: 'layout_6', sizeX: 1, sizeY: 1, row: 1, col: 1, content: '<div class="content">7</div>' },
+      { id: 'candidate', sizeX: 3, sizeY: 4, row: 0, col: 0, content: '<div class="content">1</div>' },
+      { id: 'invoice', sizeX: 3, sizeY: 1, row: 0, col: 3, content: '<div class="content">2</div>' },
+      { id: 'simple', sizeX: 3, sizeY: 1, row: 0, col: 6, content: '<div class="content">3</div>' },
+      { id: 'layout_3', sizeX: 3, sizeY: 1, row: 0, col: 9, content: '<div class="content">4</div>' },
+      { id: 'layout_4', sizeX: 3, sizeY: 3, row: 1, col: 3, content: '<div class="content">5</div>' },
+      { id: 'layout_5', sizeX: 3, sizeY: 3, row: 1, col: 6, content: '<div class="content">6</div>' },
+      { id: 'layout_6', sizeX: 3, sizeY: 6, row: 1, col: 9, content: '<div class="content">7</div>' },
+      { id: 'layout_7', sizeX: 3, sizeY: 3, row: 4, col: 0, content: '<div class="content">8</div>' },
+      { id: 'layout_8', sizeX: 3, sizeY: 3, row: 4, col: 3, content: '<div class="content">9</div>' },
+      { id: 'layout_9', sizeX: 3, sizeY: 3, row: 4, col: 6, content: '<div class="content">10</div>' },
     ]);
   }
 
@@ -29,7 +32,7 @@ export class DashboardService {
   }
 
   saveDashboard(dashboard: PanelModel[]): Observable<boolean> {
-    const panels = JSON.stringify(dashboard)
+    const panels = JSON.stringify(dashboard);
     localStorage.setItem('dashboard', panels);
     return of(true);
   }
