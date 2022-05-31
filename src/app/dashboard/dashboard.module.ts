a45ed7d90f327b4488c6e5f3007933b1edf69adb
Roman Bishko <bishko86@yahoo.com>
2022-05-31 11:43:41 +0300

    #1008 install ej2-angular-charts; ChartModule

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { DashboardLayoutModule } from '@syncfusion/ej2-angular-layouts';
+import { ChartModule, CategoryService, SplineSeriesService } from '@syncfusion/ej2-angular-charts';
 import { NgxsModule } from '@ngxs/store';
 
 import { DashboardState } from './store/dashboard.state';
@@ -12,15 +13,15 @@ import { CandidateWidgetComponent } from './widgets/candidate-widget/candidate-w
 import { InvoiceWidgetComponent } from './widgets/invoice-widget/invoice-widget.component';
 import { SimpleChartWidgetComponent } from './widgets/simple-chart-widget/simple-chart-widget.component';
 
-
 @NgModule({
   declarations: [DashboardComponent, CandidateWidgetComponent, InvoiceWidgetComponent, SimpleChartWidgetComponent],
   imports: [
     CommonModule,
     DashboardLayoutModule,
-    RouterModule.forChild([{path: '', component: DashboardComponent}]),
+    ChartModule,
+    RouterModule.forChild([{ path: '', component: DashboardComponent }]),
     NgxsModule.forFeature([DashboardState]),
   ],
-  providers: [DashboardService]
+  providers: [DashboardService, CategoryService, SplineSeriesService],
 })
-export class DashboardModule { }
+export class DashboardModule {}
