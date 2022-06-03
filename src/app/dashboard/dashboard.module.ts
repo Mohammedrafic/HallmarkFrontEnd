d8737cdda4d6cddfb474aac55ec2c8c683b014c4
Roman Bishko <bishko86@yahoo.com>
2022-06-03 11:41:45 +0300

    #1081 add @syncfusion/ej2-angular-maps pakage

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,7 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { DashboardLayoutModule } from '@syncfusion/ej2-angular-layouts';
-import { ChartModule, CategoryService, SplineSeriesService, AccumulationChartAllModule, PieSeriesService, AccumulationLegendService, AccumulationTooltipService, AccumulationDataLabelService, AccumulationAnnotationService } from '@syncfusion/ej2-angular-charts';
+import {
+  ChartModule,
+  CategoryService,
+  SplineSeriesService,
+  AccumulationChartAllModule,
+  PieSeriesService,
+  LegendService,
+} from '@syncfusion/ej2-angular-charts';
+import { MapsAllModule, MapsTooltipService } from '@syncfusion/ej2-angular-maps';
 import { NgxsModule } from '@ngxs/store';
 
 import { DashboardState } from './store/dashboard.state';
@@ -11,18 +19,19 @@ import { DashboardService } from './services/dashboard.service';
 import { DashboardComponent } from './dashboard.components';
 import { AccumulationChartComponent } from './widgets/accumulation-chart/accumulation-chart.component';
 import { ChartLineWidgetComponent } from './widgets/chart-line-widget/chart-line-widget.component';
-
+import { MapsWidgetComponent } from './widgets/maps-widget/maps-widget.component';
+import { WidgetHeaderComponent } from './widgets/widget-header/widget-header.component';
 @NgModule({
-  declarations: [DashboardComponent, ChartLineWidgetComponent, AccumulationChartComponent],
+  declarations: [DashboardComponent, ChartLineWidgetComponent, AccumulationChartComponent, MapsWidgetComponent, WidgetHeaderComponent],
   imports: [
     CommonModule,
     DashboardLayoutModule,
     ChartModule,
     AccumulationChartAllModule,
+    MapsAllModule,
     RouterModule.forChild([{ path: '', component: DashboardComponent }]),
     NgxsModule.forFeature([DashboardState]),
   ],
-  providers: [DashboardService, CategoryService, SplineSeriesService, PieSeriesService, AccumulationLegendService, AccumulationTooltipService, AccumulationDataLabelService,
-    AccumulationAnnotationService],
+  providers: [DashboardService, CategoryService, SplineSeriesService, PieSeriesService, LegendService, MapsTooltipService],
 })
 export class DashboardModule {}
