3bfaea8f8d5ad56530e57190dbb3ecc41f83bd25
Viktor Kachorovskyi <vkach@softserveinc.com>
2022-06-06 20:03:17 +0300

    [EIN-937] Create MapModule, refresh map on container resize event

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -10,28 +10,30 @@ import {
   AccumulationChartAllModule,
   PieSeriesService,
   LegendService,
+  TooltipService
 } from '@syncfusion/ej2-angular-charts';
-import { MapsAllModule, MapsTooltipService } from '@syncfusion/ej2-angular-maps';
 import { NgxsModule } from '@ngxs/store';
 
 import { DashboardState } from './store/dashboard.state';
 import { DashboardService } from './services/dashboard.service';
 import { DashboardComponent } from './dashboard.components';
 import { AccumulationChartComponent } from './widgets/accumulation-chart/accumulation-chart.component';
-import { WidgetHeaderComponent } from './widgets/widget-header/widget-header.component';
 import { LineChartComponent } from './widgets/line-chart/line-chart.component';
-import { MapsChartComponent } from './widgets/maps-chart/maps-chart.component';
+import { MapsChartModule } from './widgets/maps-chart/maps-chart.module';
+import { WidgetHeaderModule } from './widgets/widget-header/widget-header.module';
+
 @NgModule({
-  declarations: [DashboardComponent, LineChartComponent, AccumulationChartComponent, MapsChartComponent, WidgetHeaderComponent],
+  declarations: [DashboardComponent, LineChartComponent, AccumulationChartComponent],
   imports: [
     CommonModule,
     DashboardLayoutModule,
     ChartModule,
     AccumulationChartAllModule,
-    MapsAllModule,
+    MapsChartModule,
+    WidgetHeaderModule,
     RouterModule.forChild([{ path: '', component: DashboardComponent }]),
     NgxsModule.forFeature([DashboardState]),
   ],
-  providers: [DashboardService, CategoryService, SplineSeriesService, PieSeriesService, LegendService, MapsTooltipService],
+  providers: [DashboardService, CategoryService, SplineSeriesService, PieSeriesService, LegendService, TooltipService],
 })
 export class DashboardModule {}
