4c19ef005e04b18dc357f8bc8e470c36014fa42b
Roman Bishko <bishko86@yahoo.com>
2022-06-21 18:38:03 +0300

    redirect from widget position to source content

--- src/app/dashboard/widgets/position-chart/position-chart.component.ts
+++ src/app/dashboard/widgets/position-chart/position-chart.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-
+import { Router } from '@angular/router';
 import { CandidatesPositionDataModel } from '../../models/candidates-positions.model';
 
 @Component({
@@ -12,4 +12,21 @@ export class PositionChartComponent {
   @Input() public isLoading: boolean;
   @Input() public chartData: CandidatesPositionDataModel | undefined;
 
+  private mousePosition = {
+    x: 0,
+    y: 0,
+  };
+
+  constructor(private readonly router: Router) {}
+
+  public mouseDown($event: MouseEvent): void {
+    this.mousePosition.x = $event.screenX;
+    this.mousePosition.y = $event.screenY;
+  }
+
+  public toSourceContent(event: MouseEvent): void {
+    if (this.mousePosition.x === event.screenX && this.mousePosition.y === event.screenY) {
+      this.router.navigateByUrl('client/order-management');
+    }
+  }
 }
