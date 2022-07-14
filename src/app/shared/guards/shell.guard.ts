0c5620add1704ad8e8f4543e608e97d9e4bd0c74
Dmytro Prokopiuk <dproko@softserveinc.com>
2022-07-14 17:11:28 +0300

    <fix> dev

--- src/app/shared/guards/shell.guard.ts
+++ src/app/shared/guards/shell.guard.ts
@@ -2,18 +2,17 @@ import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 
 import { USER_STORAGE_KEY } from '@shared/constants/local-storage-keys';
-import { B2CAuthService } from 'src/app/b2c-auth/b2c-auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShellGuard implements CanActivate {
-  constructor(private router: Router, private b2CAuthService: B2CAuthService) { }
+  constructor(private router: Router) { }
 
   canActivate(): boolean {
     const user = window.localStorage.getItem(USER_STORAGE_KEY);
 
-    if (this.b2CAuthService.isLoggedIn() && user) {
+    if (user) {
       return true;
     }
 
