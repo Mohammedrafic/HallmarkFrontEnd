bd152aa1adc90d4c7350a7e9d5458c1dba9bb4dc
Sridhar Jonnalagadda <sridhar.jonnalagadda@hallmarkhcs.com>
2022-07-25 11:43:05 -0400

    Removing Financial Module From Menu Item

--- src/app/shared/constants/menu-config.ts
+++ src/app/shared/constants/menu-config.ts
@@ -35,11 +35,11 @@ export const MENU_CONFIG: any = {
       icon: 'lock',
       route: '',
     },
-    7: {
-      // Financial Module
-      icon: 'dollar-sign',
-      route: '',
-    },
+    //7: {
+    //  // Financial Module
+    //  icon: 'dollar-sign',
+    //  route: '',
+    //},
     8: {
       // Master Data
       icon: 'user',
@@ -170,11 +170,11 @@ export const MENU_CONFIG: any = {
       icon: 'lock',
       route: '',
     },
-    7: {
-      // Financial Module
-      icon: 'dollar-sign',
-      route: '',
-    },
+    //7: {
+    //  // Financial Module
+    //  icon: 'dollar-sign',
+    //  route: '',
+    //},
     8: {
       // Master Data
       icon: 'user',
@@ -300,11 +300,11 @@ export const MENU_CONFIG: any = {
       icon: 'lock',
       route: '',
     },
-    7: {
-      // Financial Module
-      icon: 'dollar-sign',
-      route: '',
-    },
+    //7: {
+    //  // Financial Module
+    //  icon: 'dollar-sign',
+    //  route: '',
+    //},
     8: {
       // Master Data
       icon: 'user',
