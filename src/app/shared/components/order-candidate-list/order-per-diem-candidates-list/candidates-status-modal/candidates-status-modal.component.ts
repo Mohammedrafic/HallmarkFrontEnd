adab066873b4b577f75347145167b6257c7f1ea5
Mykhailo Khrystenko <mkhrys@softserveinc.com>
2022-07-25 15:42:41 +0000

    Merged PR 14969: HIstorical Events block has been provided in Per Diem
    HIstorical Events block has been provided in Per Diem

--- src/app/shared/components/order-candidate-list/order-per-diem-candidates-list/candidates-status-modal/candidates-status-modal.component.ts
+++ src/app/shared/components/order-candidate-list/order-per-diem-candidates-list/candidates-status-modal/candidates-status-modal.component.ts
@@ -29,6 +29,8 @@ import {
   UpdateAgencyCandidateJob,
 } from '@agency/store/order-management.actions';
 import { OrderManagementState } from '@agency/store/order-management.state';
+import { AccordionOneField } from '../../../../models/accordion-one-field.model';
+import { AccordionClickArgs, ExpandEventArgs } from '@syncfusion/ej2-navigations';
 
 @Component({
   selector: 'app-candidates-status-modal',
@@ -97,9 +99,11 @@ export class CandidatesStatusModalComponent implements OnInit, OnDestroy {
   public rejectReasons: RejectReason[] = [];
   public optionFields = { text: 'statusText', value: 'applicantStatus' };
   public nextApplicantStatuses: ApplicantStatus[];
+  public orderCandidateJob: OrderCandidateJob | null;
+  public accordionClickElement: HTMLElement | null;
+  public accordionOneField: AccordionOneField;
 
   private unsubscribe$: Subject<void> = new Subject();
-  private orderCandidateJob: OrderCandidateJob | null;
   private orderApplicantsInitialData: OrderApplicantsInitialData | null;
 
   constructor(private formBuilder: FormBuilder, private store: Store, private actions$: Actions) {}
@@ -173,6 +177,16 @@ export class CandidatesStatusModalComponent implements OnInit, OnDestroy {
 
   public onAccept(): void {
     this.updateAgencyCandidateJob({ applicantStatus: ApplicantStatusEnum.Accepted, statusText: 'Accepted' });
+  }
+
+  public clickedOnAccordion(accordionClick: AccordionClickArgs): void {
+    this.accordionOneField = new AccordionOneField(this.accordionComponent);
+    this.accordionClickElement = this.accordionOneField.clickedOnAccordion(accordionClick);
+  }
+
+  public toForbidExpandSecondRow(expandEvent: ExpandEventArgs): void {
+    this.accordionOneField = new AccordionOneField(this.accordionComponent);
+    this.accordionOneField.toForbidExpandSecondRow(expandEvent, this.accordionClickElement);
   }
 
   private updateOrganizationCandidateJob(status: { applicantStatus: ApplicantStatusEnum; statusText: string }): void {
