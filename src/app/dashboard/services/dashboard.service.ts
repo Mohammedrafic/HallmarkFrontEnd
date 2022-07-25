5aa5e18024c87150fc161f9441efb0c95adee487
Roman Bishko <bishko86@yahoo.com>
2022-07-25 17:18:25 +0300

    calculation weekly or monthly time slice

--- src/app/dashboard/services/dashboard.service.ts
+++ src/app/dashboard/services/dashboard.service.ts
@@ -31,6 +31,7 @@ import type { ApplicantsByRegionDataModel } from '../models/applicants-by-region
 import type { WidgetsDataModel } from '../models/widgets-data.model';
 import { PositionTypeEnum } from '../enums/position-type.enum';
 import type {
+  ITimeSlice,
   PositionByTypeDataModel,
   PositionsByTypeAggregatedModel,
 } from '../models/positions-by-type-aggregated.model';
@@ -55,14 +56,14 @@ export class DashboardService {
   private readonly baseUrl = '/api/Dashboard';
   private readonly widgetTypeToDataMapper: Record<
     WidgetTypeEnum,
-    (filters: DashboardFiltersModel, timeSelection: TimeSelectionEnum | null) => Observable<unknown>
+    (filters: DashboardFiltersModel, timeSelection: TimeSelectionEnum) => Observable<unknown>
   > = {
     [WidgetTypeEnum.APPLICANTS_BY_REGION]: (filters: DashboardFiltersModel) => this.getApplicantsByRegionWidgetData(filters),
     [WidgetTypeEnum.ACTIVE_POSITIONS]: (filters: DashboardFiltersModel) => this.getActivePositionWidgetData(filters),
     [WidgetTypeEnum.CANDIDATES]: (filters: DashboardFiltersModel) => this.getCandidatesWidgetData(filters),
     [WidgetTypeEnum.FILLED_POSITIONS_TREND]: (filters: DashboardFiltersModel) => this.getFilledPositionTrendWidgetData(filters),
     [WidgetTypeEnum.IN_PROGRESS_POSITIONS]: (filters: DashboardFiltersModel) => this.getOrderPositionWidgetData(filters, OrderStatus.InProgress),
-    [WidgetTypeEnum.POSITIONS_BY_TYPES]: (filters: DashboardFiltersModel, timeSelection: TimeSelectionEnum | null) => this.getPositionsByTypes(filters, timeSelection),
+    [WidgetTypeEnum.POSITIONS_BY_TYPES]: (filters: DashboardFiltersModel, timeSelection: TimeSelectionEnum) => this.getPositionsByTypes(filters, timeSelection),
     [WidgetTypeEnum.FILLED_POSITIONS]: (filters: DashboardFiltersModel) => this.getOrderPositionWidgetData(filters, OrderStatus.Filled),
     [WidgetTypeEnum.OPEN_POSITIONS]: (filters,) => this.getOrderPositionWidgetData(filters, OrderStatus.Open),
     [WidgetTypeEnum.INVOICES]: () => this.getInvocesWidgetData(),
@@ -195,8 +196,8 @@ export class DashboardService {
       .pipe(map((panels) => JSON.parse(panels.state)));
   }
 
-  private getPositionsByTypes(filter: DashboardFiltersModel, timeSelection: TimeSelectionEnum | null): Observable<PositionsByTypeAggregatedModel> {
-    const timeRanges = this.calculateTimeRanges();
+  private getPositionsByTypes(filter: DashboardFiltersModel, timeSelection: TimeSelectionEnum): Observable<PositionsByTypeAggregatedModel> {
+    const timeRanges = this.calculateTimeRanges(timeSelection);
     return this.httpClient
       .post<PositionsByTypeResponseModel>(`${this.baseUrl}/getopenclosedonboardamount`, { ...timeRanges, ...filter })
       .pipe(
@@ -210,11 +211,27 @@ export class DashboardService {
       );
   }
 
-  private calculateTimeRanges(): { startDate: string; endDate: string } {
+  private getWeeksTimeRanges(week: number): ITimeSlice {
+    const numberWeek = week * 7;
+    const today = new Date();
+    const startDate = this.getDateAsISOString(new Date(today.getFullYear(), today.getMonth(), today.getDate() - numberWeek).getTime());
+    const endDate = this.getDateAsISOString(new Date(today.getFullYear(), today.getMonth(), today.getDate()).getTime());
+    return {startDate, endDate};
+  }
+
+  private getMonthTimeRanges(month: number): ITimeSlice{
     const date = new Date();
-    const startDate = this.getDateAsISOString(date.setMonth(date.getMonth() - 3));
-    const endDate = this.getDateAsISOString(date.setMonth(date.getMonth() + 6));
+    const startDate = this.getDateAsISOString(date.setMonth(date.getMonth() - month + 1));
+    const endDate = this.getDateAsISOString(date.setMonth(date.getMonth() + month));
     return { startDate, endDate };
+  }
+
+  private calculateTimeRanges(timeSelection: TimeSelectionEnum) {
+    if(timeSelection === TimeSelectionEnum.Weekly) {
+      return this.getWeeksTimeRanges(5);
+    } else {
+      return this.getMonthTimeRanges(5);
+    }  
   }
 
   private getDateAsISOString(timestamp: number): string {
