a65f3c8d85f6d9103acba4b76287cd95975af208
Volodymyr Babii <vbabi@softserveinc.com>
2022-08-01 07:08:01 +0000

    Merged PR 15380: User does not remain on the previously selected tab

--- src/app/client/order-management/order-candidates-container/order-candidates-container.component.ts
+++ src/app/client/order-management/order-candidates-container/order-candidates-container.component.ts
@@ -7,13 +7,13 @@ import { DestroyableDirective } from '@shared/directives/destroyable.directive';
 import { CandidateListEvent, OrderCandidatesListPage } from '@shared/models/order-management.model';
 import { Order } from '@shared/models/order-management.model';
 import { Observable, takeUntil } from 'rxjs';
-import { OrderType } from "@shared/enums/order-type";
+import { OrderType } from '@shared/enums/order-type';
 import { OrderManagementService } from '../order-management-content/order-management.service';
 
 @Component({
   selector: 'app-order-candidates-container',
   templateUrl: './order-candidates-container.component.html',
-  styleUrls: ['./order-candidates-container.component.scss']
+  styleUrls: ['./order-candidates-container.component.scss'],
 })
 export class OrderCandidatesContainerComponent extends DestroyableDirective implements OnInit, OnDestroy {
   public order: Order;
@@ -37,18 +37,26 @@ export class OrderCandidatesContainerComponent extends DestroyableDirective impl
   }
 
   ngOnInit(): void {
-    this.orderCandidatePage$.pipe(takeUntil((this.destroy$))).subscribe((order) => {
+    this.orderCandidatePage$.pipe(takeUntil(this.destroy$)).subscribe((order) => {
       this.orderCandidatePage = order;
       this.orderCandidates = {
         isClosed: Boolean(this.order?.orderCloseDate) || Boolean(this.order?.orderClosureReason),
-        orderId: this.order.id,
-        organizationId: this.order.organizationId as number
-      }
+        orderId: this.order?.id,
+        organizationId: this.order?.organizationId as number,
+      };
     });
   }
 
-  public onGetCandidatesList(event: CandidateListEvent) : void {
+  public onGetCandidatesList(event: CandidateListEvent): void {
     this.orderManagementService.excludeDeployed = event.excludeDeployed;
-    this.store.dispatch(new GetAgencyOrderCandidatesList(event.orderId, event.organizationId, event.currentPage, event.pageSize, event.excludeDeployed));
+    this.store.dispatch(
+      new GetAgencyOrderCandidatesList(
+        event.orderId,
+        event.organizationId,
+        event.currentPage,
+        event.pageSize,
+        event.excludeDeployed
+      )
+    );
   }
 }
