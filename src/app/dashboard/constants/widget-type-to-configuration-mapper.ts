189a6400c4691ae59857062377a68fc0129fb9a3
Roman Bishko <bishko86@yahoo.com>
2022-08-02 16:17:40 +0300

    fix Position Tred legend position

--- src/app/dashboard/constants/widget-type-to-configuration-mapper.ts
+++ src/app/dashboard/constants/widget-type-to-configuration-mapper.ts
@@ -6,7 +6,7 @@ export const widgetTypeToConfigurationMapper: Record<WidgetTypeEnum, PanelModel>
   [WidgetTypeEnum.APPLICANTS_BY_REGION]: { sizeX: 3, sizeY: 3, maxSizeY: 3, maxSizeX: 3, minSizeX: 3, minSizeY: 3 },
   [WidgetTypeEnum.APPLICANTS_BY_POSITIONS]: { sizeX: 3, sizeY: 3, maxSizeY: 3, maxSizeX: 3, minSizeX: 3, minSizeY: 3 },
   [WidgetTypeEnum.ACTIVE_POSITIONS]: { sizeX: 3, sizeY: 3, maxSizeY: 3, maxSizeX: 3, minSizeX: 3, minSizeY: 3 },
-  [WidgetTypeEnum.POSITIONS_BY_TYPES]: { sizeX: 3, sizeY: 2, maxSizeY: 2, maxSizeX: 3, minSizeX: 3, minSizeY: 2 },
+  [WidgetTypeEnum.POSITIONS_BY_TYPES]: { sizeX: 3, sizeY: 3, maxSizeY: 3, maxSizeX: 3, minSizeX: 3, minSizeY: 3 },
   [WidgetTypeEnum.IN_PROGRESS_POSITIONS]: { sizeX: 3, sizeY: 1, maxSizeX: 3, maxSizeY: 1, minSizeX: 3, minSizeY: 1 },
   [WidgetTypeEnum.OPEN_POSITIONS]: { sizeX: 3, sizeY: 1, maxSizeX: 3, maxSizeY: 1, minSizeX: 3, minSizeY: 1 },
   [WidgetTypeEnum.FILLED_POSITIONS]: { sizeX: 3, sizeY: 1, maxSizeX: 3, maxSizeY: 1, minSizeX: 3, minSizeY: 1 },
