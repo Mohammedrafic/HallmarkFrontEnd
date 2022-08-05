6969120204e28abc18f807750a99de2d1e5af25f
Manojreddy Gudi <manojreddy.gudi@hallmarkhcs.com>
2022-08-05 22:42:03 +0530

    hooked up API get call for special project

--- src/app/shared/models/special-project.model.ts
+++ src/app/shared/models/special-project.model.ts
@@ -1,20 +1,21 @@
 import { PageOfCollections } from '@shared/models/page.model';
 
 export class SpecialProject {
-  id: number;
-  category?: string;
-  name?: string;
-  regionId: number;
-  regionName: string;
-  locationId: number;
-  locationName: string;
-  departmentId: number;
-  departmentName: string;
-  skillId: number;
-  skillName: string;
-  budget?: number;
-  startDate?: Date;
-  endDate?: Date;
+    id: number;
+    name: string;
+    organizationId:number;
+    projectTypeId? :number;
+    regionId:number;
+    regionName:String;
+    locationId:number;
+    locationName :string;
+    departmentId:number;
+    departmentName :string;
+    skillId:number;
+    projectBudget : number;
+    startDate :Date;
+    endDate: Date;
+    isDeleted :boolean;
 }
 
 export type SpecialProjectPage = PageOfCollections<SpecialProject>;
