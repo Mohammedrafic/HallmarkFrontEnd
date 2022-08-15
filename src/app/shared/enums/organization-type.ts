93615224294bf4acf3851246514766ef7209c98a
Madhusudanarao Babburi <madhusudanarao.babburi@hallmarkhcs.com>
2022-08-15 17:55:56 -0400

    Task EIN-2380:1.UI Changes for Organization Type, Contact Type fields. 2.Removed Select Purchase Order by,Send Document to Agency,Timesheet Submitted by,consider Lunch Fields from UI.

---
+++ src/app/shared/enums/organization-type.ts
@@ -0,0 +1,1 @@
+export const OrganizationTypes = ['Clinical', 'Non-Clinical'];
--- src/app/shared/models/organization.model.ts
+++ src/app/shared/models/organization.model.ts
@@ -34,12 +34,7 @@ export class Organization {
     this.contactDetails = contactDetails;
     this.contactDetails.forEach((contact: ContactDetails) => contact.organizationId = organizationId || 0);
     this.preferences = preferences;
-    this.preferences.purchaseOrderBy = parseInt(this.preferences.purchaseOrderBy as string, 10);
-    this.preferences.timesheetSubmittedBy = parseInt(this.preferences.timesheetSubmittedBy as string, 10);
-    this.preferences.paymentOptions = parseInt(this.preferences.paymentOptions as string, 10);
-    if (this.preferences.timePeriodInMins === '') {
-      this.preferences.timePeriodInMins = null;
-    }
+    this.preferences.paymentOptions = parseInt(this.preferences.paymentOptions as string, 10);    
     this.preferences.organizationId = organizationId || 0;
     this.dbConnectionStringName = dataBaseConnection;
   }
@@ -51,6 +46,7 @@ export class GeneralInformation {
   externalId?: number | string | null;
   taxId: string;
   name: string;
+  organizationType: string;
   addressLine1: string;
   addressLine2: string;
   state: string;
@@ -96,12 +92,8 @@ export class Preferences {
   organizationId?: number;
   paymentOptions: number | string;
   paymentDescription: string;
-  timesheetSubmittedBy: number | string;
   weekStartsOn: number;
   considerLunch: boolean;
-  purchaseOrderBy: number | string;
-  sendDocumentToAgency: SendDocumentAgency;
-  timePeriodInMins: number | string | null;
 }
 
 export class OrganizationPage {
