d546253f056216af0cd8bdd90d95fd4bfc0d945f
Dmytro Yermolaiev <dyermol@softserveinc.com>
2022-08-19 11:49:54 +0000

    Merged PR 16682: disable uploader if settings is off
    disable uploader if settings is off

---
+++ src/app/core/helpers/settings.helper.ts
@@ -0,0 +1,11 @@
+import { SettingsKeys } from "@shared/enums/settings";
+import { OrganizationSettingsGet } from "@shared/models/organization-settings.model";
+
+export class SettingsHelper {
+  public static mapSettings(settings: OrganizationSettingsGet[]): {[key in SettingsKeys]?: OrganizationSettingsGet} {
+    return settings.reduce((acc: {[key in SettingsKeys]?: OrganizationSettingsGet}, curr: OrganizationSettingsGet)=> {
+      curr.value = (curr.value === 'true');
+      return ((acc as any)[curr.settingKey] = curr, acc)
+    }, {});
+  }
+}
