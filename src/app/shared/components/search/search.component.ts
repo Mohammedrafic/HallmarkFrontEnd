1d01a23b38777e5ec2c983a438797ad128faef2d
Denys Sokol <dsoko@softserveinc.com>
2022-09-05 13:36:20 +0000

    Merged PR 17745: EIN-4417: search placeholder
    EIN-4417: search placeholder

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -7,6 +7,9 @@ import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular
 })
 export class SearchComponent  {
   @ViewChild('inputWithIcon') search: ElementRef;
+
+  @Input() placeholder: string = 'Search';
+
   @Output() inputKeyUpEnter = new EventEmitter();
 
   constructor() { }
