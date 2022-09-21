aa8c8d30c4b10abaedc4ec7ea1941083a5e07f5d
Madhusudanarao Babburi <madhusudanarao.babburi@hallmarkhcs.com>
2022-09-21 17:49:05 -0400

    Changes for Client Finance report

--- src/app/admin/analytics/client-finance-report/client-finance-report.component.ts
+++ src/app/admin/analytics/client-finance-report/client-finance-report.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngxs/store';
+import { LogiReportComponent } from '@shared/components/logi-report/logi-report.component';
+import { LogiReportTypes } from '@shared/enums/logi-report-type.enum';
+import { LogiReportFileDetails } from '@shared/models/logi-report-file';
 import { SetHeaderState } from '../../../store/app.actions';
 
 @Component({
@@ -8,12 +11,18 @@ import { SetHeaderState } from '../../../store/app.actions';
   styleUrls: ['./client-finance-report.component.scss']
 })
 export class ClientFinanceReportComponent implements OnInit {
+  
   public title: string = "Client Finance Report";
-  constructor(private store: Store) {
-    this.store.dispatch(new SetHeaderState({ title: this.title, iconName: '' }));
-  }
-
+  public paramsData: any = {
+  };
+  public reportName: LogiReportFileDetails = { name: "/ClientFinanceReport/ClientFinanceReport.cls" };
+  public catelogName: LogiReportFileDetails = { name: "/ClientFinanceReport/Dashbord.cat" };
+  public reportType: LogiReportTypes = LogiReportTypes.WebReport;
+  
   ngOnInit(): void {
+   
   }
+ 
+     
 
 }
