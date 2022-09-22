76c88a9a213e8fca02b8dca7c699ba7536f17798
Ihor_Mahera <ihor.mahera@apollolims.com>
2022-09-22 11:11:40 +0300

    fix link to the order

--- src/app/client/order-management/order-management-content/order-management-content.component.ts
+++ src/app/client/order-management/order-management-content/order-management-content.component.ts
@@ -263,6 +263,7 @@ export class OrderManagementContentComponent extends AbstractGridConfigurationCo
   private prefix: string | null;
   private orderId: number | null;
   private creatingReorder = false;
+  private stateFullOrderId: string | null;
   public readonly formatDate = formatDate;
   public readonly placeholderDate = placeholderDate;
 
@@ -335,8 +336,9 @@ export class OrderManagementContentComponent extends AbstractGridConfigurationCo
 
     this.onSelectedOrderDataLoadHandler();
 
-    const locationState = this.location.getState() as { orderId: number };
+    const locationState = this.location.getState() as { orderId: number, fullOrderId: string };
     this.previousSelectedOrderId = locationState.orderId;
+    this.stateFullOrderId = locationState.fullOrderId;
 
     this.onGridPageChangedHandler();
     this.onOrganizationChangedHandler();
@@ -452,8 +454,12 @@ export class OrderManagementContentComponent extends AbstractGridConfigurationCo
       case OrganizationOrderManagementTabs.AllOrders:
         this.filters.isTemplate = false;
         this.filters.includeReOrders = true;
+
+        this.setFullOrderIdData();
         this.hasOrderAllOrdersId();
-        this.store.dispatch([new GetOrders(this.filters), new GetOrderFilterDataSources()]);
+
+        this.store.dispatch([new GetOrders(this.filters), new GetOrderFilterDataSources()])
+          .subscribe(() => this.handleFullOrderId());
         break;
       case OrganizationOrderManagementTabs.PerDiem:
         this.filters.orderTypes = [OrderType.OpenPerDiem];
@@ -695,7 +701,7 @@ export class OrderManagementContentComponent extends AbstractGridConfigurationCo
     this.rowSelected(event, this.gridWithChildRow);
 
     if (!event.isInteracted) {
-      if (event.data.isTemplate) {
+      if (event.data?.isTemplate) {
         this.navigateToOrderTemplateForm();
         this.store.dispatch(new GetSelectedOrderById(event.data.id));
       } else {
@@ -1514,5 +1520,24 @@ export class OrderManagementContentComponent extends AbstractGridConfigurationCo
 
   private getProjectSpecialData(): void {
     this.store.dispatch(new GetProjectSpecialData());
+  }
+
+  private setFullOrderIdData(): void {
+    if (this.stateFullOrderId) {
+      this.filters.orderPublicId = this.stateFullOrderId;
+      this.OrderFilterFormGroup.controls['orderPublicId'].setValue(this.filters.orderPublicId);
+    }
+  }
+
+  private handleFullOrderId(): void {
+    if (this.stateFullOrderId) {
+      const [ data ] = this.store.selectSnapshot(OrderManagementContentState.ordersPage)?.items || [];
+
+      if (data) {
+        this.onRowClick({ data });
+        this.filteredItems = this.filterService.generateChips(this.OrderFilterFormGroup, this.filterColumns)
+        this.stateFullOrderId = null;
+      }
+    }
   }
 }
