9f30e36b372fdc33c99e3c015027744bf45a29ee
Vemunuri Thirupathi <vemunuri.thirupathi@hallmarkhcs.com>
2022-09-23 23:30:48 +0530

    completed design the filter dailog on five reports

--- src/app/admin/analytics/job-details/job-details.component.ts
+++ src/app/admin/analytics/job-details/job-details.component.ts
@@ -9,7 +9,7 @@ import { Region, regionFilter } from '@shared/models/region.model';
 import { Department, DepartmentsByLocationsFilter } from '@shared/models/department.model';
 import { ChangeEventArgs, FieldSettingsModel } from '@syncfusion/ej2-angular-dropdowns';
 import { filter, Observable, Subject, takeUntil } from 'rxjs';
-import { SetHeaderState } from 'src/app/store/app.actions';
+import { SetHeaderState, ShowFilterDialog } from 'src/app/store/app.actions';
 import { ControlTypes, ValueType } from '@shared/enums/control-types.enum';
 import { UserState } from 'src/app/store/user.state';
 import { BUSINESS_DATA_FIELDS } from '@admin/alerts/alerts.constants';
@@ -22,6 +22,9 @@ import { LogiReportState } from '@organization-management/store/logi-report.stat
 import { startDateValidator } from '@shared/validators/date.validator';
 import { formatDate } from '@angular/common';
 import { LogiReportComponent } from '@shared/components/logi-report/logi-report.component';
+import { FilteredItem } from '@shared/models/filter.model';
+import { FilterService } from '@shared/services/filter.service';
+import { analyticsConstants } from '../constants/analytics.constant';
 @Component({
   selector: 'app-page-report',
   templateUrl: './job-details.component.html',
@@ -80,10 +83,15 @@ export class JobDetailsComponent implements OnInit {
   public defaultLocations:(number|undefined)[]=[];
   public defaultDepartments:(number|undefined)[]=[];
   public today = new Date();
+  public filteredItems: FilteredItem[] = [];
+  public isClearAll: boolean = false;
+  public isInitialLoad: boolean = false;
   @ViewChild(LogiReportComponent, { static: true }) logiReportComponent: LogiReportComponent;
   constructor(private store: Store,
-    private formBuilder: FormBuilder) {
+    private formBuilder: FormBuilder,
+    private filterService: FilterService  ) {
     this.store.dispatch(new SetHeaderState({ title: this.title, iconName: '' }));
+    this.initForm();
     const user = this.store.selectSnapshot(UserState.user);
     if (user?.businessUnitType != null) {
       this.store.dispatch(new GetBusinessByUnitType(BusinessUnitType.Organization));
@@ -91,58 +99,75 @@ export class JobDetailsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-   
+    this.isInitialLoad = true;
+    this.orderFilterColumnsSetup();
+    this.onFilterControlValueChangedHandler();
+  }
+
+  private initForm(): void {
     let startDate = new Date(Date.now());
-     startDate.setDate(startDate.getDate() -90);
+    startDate.setDate(startDate.getDate() - 90);
     this.jobDetailsForm = this.formBuilder.group(
       {
-        business: new FormControl(null,[Validators.required]),
-        startDate:new FormControl(startDate,[Validators.required]),
-        endDate: new FormControl(new Date(Date.now()),[Validators.required]),
-        regionId: new FormControl(null,[Validators.required]),
-        locationId: new FormControl(null,[Validators.required]),
-        departmentId: new FormControl(null,[Validators.required])
-
+        businessIds: new FormControl(null, [Validators.required]),
+        startDate: new FormControl(startDate, [Validators.required]),
+        endDate: new FormControl(new Date(Date.now()), [Validators.required]),
+        regionIds: new FormControl(null, [Validators.required]),
+        locationIds: new FormControl(null, [Validators.required]),
+        departmentIds: new FormControl(null, [Validators.required])
       }
     );
-    this.orderFilterColumnsSetup();
-    this.bussinessControl = this.jobDetailsForm.get('business') as AbstractControl;
+  }
+
+  public onFilterControlValueChangedHandler(): void {
+    this.bussinessControl = this.jobDetailsForm.get(analyticsConstants.formControlNames.BusinessIds) as AbstractControl;
     this.businessData$.pipe(takeUntil(this.unsubscribe$)).subscribe((data) => {
       this.organizations = data;
       this.filterColumns.businessIds.dataSource = data;
-      this.defaultOrganizations=data.map((list) => list.id);
+      this.defaultOrganizations = data.map((list) => list.id);
     });
-   
     this.bussinessControl.valueChanges.pipe(takeUntil(this.unsubscribe$)).subscribe((data) => {
-
-      this.selectedOrganizations = this.organizations?.filter((x) => data?.includes(x.id));
-      let regionFilter: regionFilter = {
-        ids: data,
-        getAll: true
-      };
-      this.store.dispatch(new GetRegionsByOrganizations(regionFilter));
+      if (!this.isClearAll) {
+        this.selectedOrganizations = this.organizations?.filter((x) => data?.includes(x.id));
+        let regionFilter: regionFilter = {
+          ids: data,
+          getAll: true
+        };
+        this.store.dispatch(new GetRegionsByOrganizations(regionFilter));
+      }
+      else {
+        this.isClearAll = false;
+      }
     });
-    this.regionIdControl = this.jobDetailsForm.get('regionId') as AbstractControl;
+    this.regionIdControl = this.jobDetailsForm.get(analyticsConstants.formControlNames.RegionIds) as AbstractControl;
     this.regionIdControl.valueChanges.pipe(takeUntil(this.unsubscribe$)).subscribe((data) => {
-      this.selectedRegions = this.regions?.filter((object) => data?.includes(object.id));
-      let locationFilter: LocationsByRegionsFilter = {
-        ids: data,
-        getAll: true
-      };
-      this.store.dispatch(new GetLocationsByRegions(locationFilter));
+      if (this.regionIdControl.value.length > 0) {
+        this.selectedRegions = this.regions?.filter((object) => data?.includes(object.id));
+        let locationFilter: LocationsByRegionsFilter = {
+          ids: data,
+          getAll: true
+        };
+        this.store.dispatch(new GetLocationsByRegions(locationFilter));
+      }
     });
-    this.locationIdControl = this.jobDetailsForm.get('locationId') as AbstractControl;
+    this.locationIdControl = this.jobDetailsForm.get(analyticsConstants.formControlNames.LocationIds) as AbstractControl;
     this.locationIdControl.valueChanges.pipe(takeUntil(this.unsubscribe$)).subscribe((data) => {
-      this.selectedLocations = this.locations?.filter((object) => data?.includes(object.id));
-      let departmentFilter: DepartmentsByLocationsFilter = {
-        ids: data,
-        getAll: true
-      };
-      this.store.dispatch(new GetDepartmentsByLocations(departmentFilter));
+      if (this.locationIdControl.value.length > 0) {
+        this.selectedLocations = this.locations?.filter((object) => data?.includes(object.id));
+        let departmentFilter: DepartmentsByLocationsFilter = {
+          ids: data,
+          getAll: true
+        };
+        this.store.dispatch(new GetDepartmentsByLocations(departmentFilter));
+      }
     });
-    this.departmentIdControl = this.jobDetailsForm.get('departmentId') as AbstractControl;
+    this.departmentIdControl = this.jobDetailsForm.get(analyticsConstants.formControlNames.DepartmentIds) as AbstractControl;
     this.departmentIdControl.valueChanges.pipe(takeUntil(this.unsubscribe$)).subscribe((data) => {
-      this.selectedDepartments = this.departments?.filter((object) => data?.includes(object.departmentId));    
+      this.selectedDepartments = this.departments?.filter((object) => data?.includes(object.departmentId));
+      if (this.isInitialLoad) {
+        this.isInitialLoad = false;
+        this.SearchReport();
+      }
     });
     this.onOrganizationsChange();
     this.onRegionsChange();
@@ -150,11 +175,6 @@ export class JobDetailsComponent implements OnInit {
   }
  
   public SearchReport(): void {
-    this.jobDetailsForm.markAllAsTouched();
-    if (this.jobDetailsForm?.invalid) {
-      return;
-    }
-   
       let { startDate, endDate } = this.jobDetailsForm.getRawValue();
       this.paramsData =
       {
@@ -195,8 +215,8 @@ export class JobDetailsComponent implements OnInit {
         type: ControlTypes.Multiselect,
         valueType: ValueType.Id,
         dataSource: [],
-        valueField: 'name',
-        valueId: 'id',
+        valueField: 'departmentName',
+        valueId: 'departmentId',
       },
       startDate: { type: ControlTypes.Date, valueType: ValueType.Text },
       endDate: { type: ControlTypes.Date, valueType: ValueType.Text }
@@ -237,5 +257,31 @@ export class JobDetailsComponent implements OnInit {
           this.defaultDepartments=data.map((list) => list.departmentId);
         }
       });
-  }  
+  }
+  public showFilters(): void {
+    this.onFilterControlValueChangedHandler();
+    this.store.dispatch(new ShowFilterDialog(true));
+  }
+  public onFilterDelete(event: FilteredItem): void {
+    this.filterService.removeValue(event, this.jobDetailsForm, this.filterColumns);
+  }
+  public onFilterClearAll(): void {
+    this.isClearAll = true;
+    this.jobDetailsForm.get(analyticsConstants.formControlNames.BusinessIds)?.setValue([]);
+    this.jobDetailsForm.get(analyticsConstants.formControlNames.RegionIds)?.setValue([]);
+    this.jobDetailsForm.get(analyticsConstants.formControlNames.LocationIds)?.setValue([]);
+    this.jobDetailsForm.get(analyticsConstants.formControlNames.DepartmentIds)?.setValue([]);
+    this.jobDetailsForm.get(analyticsConstants.formControlNames.StartDate)?.setValue(null);
+    this.jobDetailsForm.get(analyticsConstants.formControlNames.EndDate)?.setValue(null);
+    this.filteredItems = [];
+  }
+  public onFilterApply(): void {
+    this.jobDetailsForm.markAllAsTouched();
+    if (this.jobDetailsForm?.invalid) {
+      return;
+    }
+    this.filteredItems = this.filterService.generateChips(this.jobDetailsForm, this.filterColumns);
+    this.SearchReport();
+    this.store.dispatch(new ShowFilterDialog(false));
+  }
 }
