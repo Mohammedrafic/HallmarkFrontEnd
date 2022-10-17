17b27e6306c464b1a92e625e3a895470ee4b5892
Manojreddy Gudi <manojreddy.gudi@hallmarkhcs.com>
2022-10-17 14:58:04 -0400

    Help Icon added with tooltip. Aligned position widget contents to center as per feedback

--- src/app/dashboard/widgets/widget-legend/widget-legend.component.ts
+++ src/app/dashboard/widgets/widget-legend/widget-legend.component.ts
@@ -13,6 +13,7 @@ export class WidgetLegendComponent {
   @Input() public legendData: WidgetLegengDataModel[];
   @Input() public showPercentRatio: boolean = false;
   @Input() public legendPosition: LegendPositionEnum = LegendPositionEnum.Bottom;
+  @Input() public description: string;
 
   @Output() onClickLegend: EventEmitter<void> = new EventEmitter();
   @Output() changeCheckbox: EventEmitter<string> = new EventEmitter();
--- src/app/dashboard/widgets/widget-wrapper/widget-wrapper.module.ts
+++ src/app/dashboard/widgets/widget-wrapper/widget-wrapper.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WidgetWrapperComponent } from './widget-wrapper.component';
 import { InlineLoaderModule } from '@shared/components/inline-loader/inline-loader.module';
+import { TooltipModule } from '@syncfusion/ej2-angular-popups';
+import { FeatherModule } from 'angular-feather';
+import { Info } from 'angular-feather/icons';
 
 @NgModule({
   declarations: [WidgetWrapperComponent],
   exports: [WidgetWrapperComponent],
-  imports: [CommonModule, InlineLoaderModule],
+  imports: [CommonModule, InlineLoaderModule, TooltipModule, FeatherModule.pick({ Info })],
 })
 export class WidgetWrapperModule {}
