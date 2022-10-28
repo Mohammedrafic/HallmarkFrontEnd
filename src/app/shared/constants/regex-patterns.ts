c5c24efc405c510afe7f5581c04f4a093e4c08ff
Andrii Paziuk <apazi@softserveinc.com>
2022-10-28 12:00:19 +0000

    Merged PR 21375: story_ein-4616_edit_validation_messages
    add validation message for some fields

--- src/app/shared/constants/regex-patterns.ts
+++ src/app/shared/constants/regex-patterns.ts
@@ -1,5 +1,6 @@
 export const ONLY_LETTERS = /^[a-zA-Z\s]*$/;
 export const CHARS_HYPHEN_APOSTROPHE = /^[a-zA-Z`'-\s]*$/;
 export const ONLY_NUMBER = /^[0-9]*$/;
+export const ONLY_NUMBER_AND_DOT = /^[0-9\.]*$/;
 export const ALPHANUMERIC = /^[a-zA-Z0-9_]+$/;
 export const ALPHANUMERICS_AND_SYMBOLS = /^([\w\s-_,.;:`@!#$%^&*)(?\\\\/|"\[\]~}{'><]*]*)(?=(.*\w){1})(?=(.*[a-zA-Z]))[a-zA-Z0-9]{3,}([\w\s-_,.;:`@!#$%^&*)(?\\\\/|"\[\]~}{'><]*]*)$/;
