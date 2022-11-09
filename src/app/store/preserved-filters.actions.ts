ea1cd9c0234fe84c2024e7ca24a7f158fb297aad
Dmytro Yermolaiev <dyermol@softserveinc.com>
2022-11-09 12:17:36 +0000

    Merged PR 22236: move timesheets filtering from agency side to separate state
    move timesheets filtering from agency side to separate state

--- src/app/store/preserved-filters.actions.ts
+++ src/app/store/preserved-filters.actions.ts
@@ -9,3 +9,8 @@ export class SetPreservedFilters {
   static readonly type = '[preserved filters] Save or update preserved filters';
   constructor(public payload: PreservedFilters | PreservedFiltersGlobal, public isGlobal: boolean = false) { }
 }
+
+export class SetPreservedFiltersForTimesheets {
+  static readonly type = '[preserved filters] Save or update preserved filters for the timesheets';
+  constructor(public payload: PreservedFilters) { }
+}
