fab6e15decb020b94fb90110422fe4153135977e
Dmytro Yermolaiev <dyermol@softserveinc.com>
2022-11-11 11:14:47 +0000

    Merged PR 22425: added classification tooltip
    added classification tooltip

---
+++ src/app/shared/components/candidate-details/candidate-grid/grid-classification-renderer/grid-classification-renderer.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { ICellRendererAngularComp } from '@ag-grid-community/angular';
+import { ICellRendererParams } from '@ag-grid-community/core';
+import { GridHelper } from '@shared/helpers/grid.helper';
+import { JobClassification } from '@shared/enums/job-classification';
+
+@Component({
+  selector: 'app-grid-classification-renderer',
+  templateUrl: './grid-classification-renderer.component.html',
+  styleUrls: ['./grid-classification-renderer.component.scss'],
+})
+export class GridClassificationRendererComponent implements ICellRendererAngularComp {
+  public cellValue: string;
+  public valueHelper = new GridHelper();
+
+  constructor() {}
+
+  public agInit(params: ICellRendererParams): void {
+    this.cellValue = this.formatClassifications(params.data.classifications);
+  }
+
+  public refresh(params: ICellRendererParams): boolean {
+    this.cellValue = this.formatClassifications(params.data.classifications);
+    return true;
+  }
+
+  public formatClassifications(classifications: number[]): string {
+    let value = '';
+    classifications.forEach((classification, index) => {
+      value += JobClassification[classification];
+      if (classifications.length !== (index + 1)) {
+        value += ', ';
+      }
+    });
+    return `${value}`
+  } 
+}
