64e131792ab02860d7a2ba6220ee7c0412daebfe
Yevhen Velychko <eveli@softserveinc.com>
2022-11-25 13:06:30 +0200

    Ein-6697 add dialog helper

--- src/app/modules/invoices/components/invoice-payment-details/invoice-payment-details.component.ts
+++ src/app/modules/invoices/components/invoice-payment-details/invoice-payment-details.component.ts
@@ -1,8 +1,5 @@
-import { AfterViewChecked, ChangeDetectionStrategy, Component,
-  EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { Destroyable } from '@core/helpers';
-import { DialogComponent } from '@syncfusion/ej2-angular-popups';
-import { debounceTime, Subject, takeUntil } from 'rxjs';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { DestroyDialog } from '@core/helpers';
 
 @Component({
   selector: 'app-invoice-payment-details',
@@ -10,34 +7,11 @@ import { debounceTime, Subject, takeUntil } from 'rxjs';
   styleUrls: ['./invoice-payment-details.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class InvoicePaymentDetailsComponent extends Destroyable implements AfterViewChecked, OnInit {
-  @ViewChild('paymenDetailsDialog') paymenDetailsDialog: DialogComponent;
-
-  @Output() destroyDialog: EventEmitter<void> = new EventEmitter();
-
-  private readonly viewObserver: Subject<void> = new Subject();
+export class InvoicePaymentDetailsComponent extends DestroyDialog implements OnInit {
 
   ngOnInit(): void {
     this.watchForCloseStream();
   }
 
-  ngAfterViewChecked(): void {
-    this.paymenDetailsDialog.show();
-  }
-
-  closeDetails(): void {
-    this.paymenDetailsDialog.hide();
-    this.viewObserver.next();
-  }
-
-  private watchForCloseStream(): void {
-    this.viewObserver.asObservable()
-    .pipe(
-      debounceTime(200),
-      takeUntil(this.componentDestroy()),
-    )
-    .subscribe(() => {
-      this.destroyDialog.emit();
-    });
-  }
+  closePaymentDialog() {}
 }
