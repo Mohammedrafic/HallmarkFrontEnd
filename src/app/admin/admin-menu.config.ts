736b8d1d02a8e428387c5a3e9411623204710de8
Vadym Khytryk <vkhyt@softserveinc.com>
2022-12-15 10:53:24 +0000

    Merged PR 24817: Fix issue with permissions
    Fix issue with permissions

--- src/app/admin/admin-menu.config.ts
+++ src/app/admin/admin-menu.config.ts
@@ -2,8 +2,8 @@ import { MenuSettings } from '@shared/models';
 
 export const MASTER_DATA_CONFIG: MenuSettings[] = [
   { text: 'Skills', id: 1, route: './skills' },
-  { text: 'Credentials', id: 2, route: './credentials', permissionKey: 'CanViewMasterCredentials' },
-  { text: 'Holidays', id: 3, route: './holidays', permissionKey: 'CanViewMasterHolidays' },
-  { text: 'Reasons for Rejection', id: 4, route: './reject-reason', permissionKey: 'CanOrganizationViewOrders' },
+  { text: 'Credentials', id: 2, route: './credentials', permissionKeys: ['CanViewMasterCredentials'] },
+  { text: 'Holidays', id: 3, route: './holidays', permissionKeys: ['CanViewMasterHolidays'] },
+  { text: 'Reasons for Rejection', id: 4, route: './reject-reason', permissionKeys: ['CanOrganizationViewOrders'] },
   { text: 'Manual invoice reasons', id: 5, route: './manual-invoice-reasons'},
 ];
--- src/app/organization-management/organization-management-menu.config.ts
+++ src/app/organization-management/organization-management-menu.config.ts
@@ -1,18 +1,32 @@
 import { MenuSettings } from '@shared/models';
 
 export const ORG_SETTINGS: MenuSettings[] = [
-  { text: 'Regions', id: 11, route: './regions', permissionKey: 'CanViewOrganizationalHierarchy' },
-  { text: 'Locations', id: 2, route: './locations', permissionKey: 'CanViewOrganizationalHierarchy' },
-  { text: 'Departments', id: 1, route: './departments', permissionKey: 'CanViewOrganizationalHierarchy' },
-  { text: 'Shifts', id: 5, route: './shifts', permissionKey: 'CanViewShifts' },
-  { text: 'Skills', id: 3, route: './skills', permissionKey: 'CanViewAssignedSkills' },
-  { text: 'Credentials', id: 4, route: './credentials', permissionKey: 'CanViewMasterCredentials' },
-  { text: 'Holidays', id: 7, route: './holidays', permissionKey: 'CanViewOrganizationHolidays' },
-  { text: 'Workflow', id: 8, route: './workflow', permissionKey: 'CanViewWorkflows' },
-  { text: 'Bill Rates', id: 9, route: './bill-rates', permissionKey: 'CanViewSettingsBillRates' },
-  { text: 'Configurations', id: 6, route: './settings', permissionKey: 'CanViewOrganizationSettings' },
-  { text: 'Reasons', id: 10, route: './reasons', permissionKey: 'CanOrganizationViewOrders' },
-  { text: 'Special Project', id: 12, route: './specialproject', permissionKey: 'CanViewSpecialProjects' },
+  {
+    text: 'Regions',
+    id: 11,
+    route: './regions',
+    permissionKeys: ['CanViewOrganizationalHierarchy', 'CanCreateOrders'],
+  },
+  {
+    text: 'Locations',
+    id: 2,
+    route: './locations',
+    permissionKeys: ['CanViewOrganizationalHierarchy', 'CanCreateOrders'],
+  },
+  {
+    text: 'Departments',
+    id: 1, route: './departments',
+    permissionKeys: ['CanViewOrganizationalHierarchy', 'CanCreateOrders'],
+  },
+  { text: 'Shifts', id: 5, route: './shifts', permissionKeys: ['CanViewShifts'] },
+  { text: 'Skills', id: 3, route: './skills', permissionKeys: ['CanViewAssignedSkills'] },
+  { text: 'Credentials', id: 4, route: './credentials', permissionKeys: ['CanViewMasterCredentials'] },
+  { text: 'Holidays', id: 7, route: './holidays', permissionKeys: ['CanViewOrganizationHolidays'] },
+  { text: 'Workflow', id: 8, route: './workflow', permissionKeys: ['CanViewWorkflows'] },
+  { text: 'Bill Rates', id: 9, route: './bill-rates', permissionKeys: ['CanViewSettingsBillRates'] },
+  { text: 'Configurations', id: 6, route: './settings', permissionKeys: ['CanViewOrganizationSettings'] },
+  { text: 'Reasons', id: 10, route: './reasons', permissionKeys: ['CanOrganizationViewOrders'] },
+  { text: 'Special Project', id: 12, route: './specialproject', permissionKeys: ['CanViewSpecialProjects'] },
   { text: 'Business Lines', id: 13, route: './businesslines' },
-  { text: 'Tiers', id: 14, route: './tiers',  permissionKey: 'CanViewOrganizationTiers'}
+  { text: 'Tiers', id: 14, route: './tiers',  permissionKeys: ['CanViewOrganizationTiers']},
 ];
