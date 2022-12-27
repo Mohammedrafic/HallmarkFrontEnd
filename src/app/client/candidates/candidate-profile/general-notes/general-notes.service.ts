1f66308612de4a439277048101835f7baccf24c5
Andrii Paziuk <apazi@softserveinc.com>
2022-12-27 11:32:34 +0000

    Merged PR 25608: bug_ein-8713_ui_issue_with_general_notes

--- src/app/client/candidates/candidate-profile/general-notes/general-notes.service.ts
+++ src/app/client/candidates/candidate-profile/general-notes/general-notes.service.ts
@@ -4,21 +4,22 @@ import { HttpClient } from '@angular/common/http';
 import { shareReplay } from 'rxjs/operators';
 import { CategoryModel } from '@client/candidates/candidate-profile/general-notes/models/category.model';
 import { EditGeneralNoteModel, GeneralNotesModel } from '@client/candidates/candidate-profile/general-notes/general-notes.model';
+import { NavigationWrapperService } from '@shared/services/navigation-wrapper.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GeneralNotesService {
   private _sideDialogTitle$: Subject<string> = new Subject<string>();
-  private _editMode$: BehaviorSubject<EditGeneralNoteModel | null> = new BehaviorSubject<EditGeneralNoteModel | null>(null);
+  private _editMode$: BehaviorSubject<EditGeneralNoteModel | null> = new BehaviorSubject<EditGeneralNoteModel | null>(
+    null
+  );
   public notes$: BehaviorSubject<GeneralNotesModel[]> = new BehaviorSubject<GeneralNotesModel[]>([]);
 
   public sideDialogTitle$: Observable<string> = this._sideDialogTitle$.asObservable();
   public editMode$: Observable<EditGeneralNoteModel | null> = this._editMode$.asObservable();
 
-
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient, private navigationWrapperService: NavigationWrapperService) {}
 
   public setSideDialogTitle(title: string) {
     this._sideDialogTitle$.next(title);
@@ -32,6 +33,7 @@ export class GeneralNotesService {
     } else {
       this.notes$.next([...this.notes$.getValue(), note]);
     }
+    this.navigationWrapperService.areUnsavedChanges = this.hasUnsavedChanges.bind(this);
   }
 
   public deleteNote(index: number): void {
@@ -47,7 +49,18 @@ export class GeneralNotesService {
     this._editMode$.next(null);
   }
 
+  public resetNoteList(): void {
+    this.notes$.next([]);
+    this.resetEditMode();
+  }
+
   public getCategories(): Observable<CategoryModel[]> {
     return this.http.get<CategoryModel[]>('/api/employeeGeneralNotes/categories/all').pipe(shareReplay());
   }
+
+  public hasUnsavedChanges(): boolean {
+    //TODO: update logic for edit mode
+    return  this.notes$.getValue().length > 0;
+  }
+
 }
