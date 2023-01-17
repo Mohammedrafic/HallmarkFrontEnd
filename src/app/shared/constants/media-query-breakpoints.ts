22c1f1ffa1fe6cade2e414e68e62284404797dde
Roman Bishko <bishko86@yahoo.com>
2023-01-17 22:52:00 +0200

    fix issue with buttons on desktop

--- src/app/shared/constants/media-query-breakpoints.ts
+++ src/app/shared/constants/media-query-breakpoints.ts
@@ -1,4 +1,5 @@
 export const SmallTabletWidth = 490;
 export const MiddleTabletWidth = 640;
 export const SmallToolbarWidth = 700;
+export const TabletWidth = 1024;
 export const SmallDesktopWidth = 1275;
