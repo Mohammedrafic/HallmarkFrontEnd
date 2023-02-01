cd99834778f6a6c2d052ef36cf827e46ad296b45
Roman Bishko <bishko86@yahoo.com>
2023-02-01 22:31:32 +0200

    restruct NumericTextboxModule; move shared components to module scum

---
+++ src/app/shared/components/error-message/error-message.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { ValidationErrorModule } from "@shared/pipes/validation-error/validation.error.module";
+import { ErrorMessageComponent } from "./error-message.component";
+
+@NgModule({
+  declarations: [ErrorMessageComponent],
+  exports: [ErrorMessageComponent],
+  imports: [CommonModule, ValidationErrorModule] 
+})
+export class ErrorMessageModule {}
--- src/app/shared/components/form-controls/datepicker/datepicker.module.ts
+++ src/app/shared/components/form-controls/datepicker/datepicker.module.ts
@@ -6,11 +6,11 @@ import { CommonModule } from '@angular/common';
 import { DatepickerComponent } from '@shared/components/form-controls/datepicker/datepicker.component';
 import { FormControlWrapperModule } from '@shared/components/form-controls/form-control-wrapper/form-control-wrapper.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from '@shared/shared.module';
+import { ErrorMessageModule } from '@shared/components/error-message/error-message.module';
 
 @NgModule({
   declarations: [DatepickerComponent],
-  imports: [CommonModule, FormControlWrapperModule, ReactiveFormsModule, DatePickerModule, SharedModule],
+  imports: [CommonModule, FormControlWrapperModule, ReactiveFormsModule, DatePickerModule, ErrorMessageModule],
   exports: [DatepickerComponent],
   providers: [MaskedDateTimeService],
 })
