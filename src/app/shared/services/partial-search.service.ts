043a9a1e4495692cce54c13a47d582ca187df5a2
Roman Bishko <bishko86@yahoo.com>
2023-02-06 18:49:16 +0200

    move iteration to separate method;

--- src/app/shared/services/partial-search.service.ts
+++ src/app/shared/services/partial-search.service.ts
@@ -1,9 +1,9 @@
-
-import { FieldSettingsModel } from "@syncfusion/ej2-angular-dropdowns";
-import { debounceTime, Subject, distinctUntilChanged } from "rxjs";
+import { FieldSettingsModel } from '@syncfusion/ej2-angular-dropdowns';
+import { debounceTime, Subject, distinctUntilChanged } from 'rxjs';
 
 export class PartilSearchService {
-  private filter$: Subject<{ dataSource: unknown[], searchString: string, options: FieldSettingsModel }> = new Subject();
+  private filter$: Subject<{ dataSource: unknown[]; searchString: string; options: FieldSettingsModel }> =
+    new Subject();
   private result$: Subject<unknown[]> = new Subject();
 
   constructor() {
@@ -11,13 +11,13 @@ export class PartilSearchService {
   }
 
   private subscribeToFilter(): void {
-    this.filter$.pipe(debounceTime(400), distinctUntilChanged
-      ((prev, curr) => prev.searchString === curr.searchString)).subscribe(({ dataSource, searchString, options }) => {
-        const result = dataSource.filter((data: any) => {
-          const itemValue = data[options.text!]?.trim()?.toLowerCase();
-          const searchValue = searchString.trim().toLowerCase();
-          return itemValue.includes(searchValue);
-        });
+    this.filter$
+      .pipe(
+        debounceTime(400),
+        distinctUntilChanged((prev, curr) => prev.searchString === curr.searchString)
+      )
+      .subscribe((data) => {
+        const result = this.filterItemsBySubString(data);
         this.result$.next(result);
       });
   }
@@ -25,5 +25,21 @@ export class PartilSearchService {
   public search(dataSource: unknown[], searchString: string, options: FieldSettingsModel): Subject<unknown[]> {
     this.filter$.next({ dataSource, searchString, options });
     return this.result$;
+  }
+
+  private filterItemsBySubString({
+    dataSource,
+    searchString,
+    options,
+  }: {
+    dataSource: unknown[];
+    searchString: string;
+    options: FieldSettingsModel;
+  }): unknown[] {
+    return dataSource.filter((data: unknown) => {
+      const itemValue = (data as Record<string, string>)[options.text!]?.trim()?.toLowerCase();
+      const searchValue = searchString.trim().toLowerCase();
+      return itemValue.includes(searchValue);
+    });
   }
 }
