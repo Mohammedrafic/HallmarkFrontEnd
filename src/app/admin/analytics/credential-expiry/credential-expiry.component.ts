1d2ddcf00d60a2a5cd853d04867ec1b3336e3706
Nambi Ramamoorthy <nambi@hallmarkhcs.com>
2023-02-08 14:44:48 +0530

    ein_9706 credentialExpiry canidatesStatus fixed

--- src/app/admin/analytics/credential-expiry/credential-expiry.component.ts
+++ src/app/admin/analytics/credential-expiry/credential-expiry.component.ts
@@ -160,11 +160,14 @@ export class CredentialExpiryComponent implements OnInit,OnDestroy {
         if (data != null) {
           this.isAlive = false;
           this.filterOptionsData = data;
-          this.filterColumns.candidateStatuses.dataSource = data.candidateStatuses;
-          this.filterColumns.agencyIds.dataSource = data.agencies;
-          this.defaultCandidateStatuses = data.candidateStatuses.map((list) => list.status);
-          this.defaultAgencys = data.agencies.map((list) => list.agencyId);
-
+          let notLoadingStatuses :string[]= ['Applied','Shortlisted','Rejected','Bill Rate Pending','Offered Bill Rate','Offboard','Withdraw','Cancelled','Not Applied'];
+          
+          data.candidateStatuses=(data.candidateStatuses||[]).filter(f=>!(notLoadingStatuses).includes(f.statusText)) ;
+            this.filterColumns.candidateStatuses.dataSource = data.candidateStatuses;
+              this.filterColumns.agencyIds.dataSource = data.agencies;
+             this.defaultCandidateStatuses = (data.candidateStatuses||[]).map((list) => list.status);
+             this.defaultAgencys = data.agencies.map((list) => list.agencyId);
+              this.credentialExpiryForm.controls["candidateStatuses"].setValue(this.defaultCandidateStatuses);
           if (this.isInitialLoad) {
             setTimeout(() => { this.SearchReport(); }, 3000)
             this.isInitialLoad = false;
