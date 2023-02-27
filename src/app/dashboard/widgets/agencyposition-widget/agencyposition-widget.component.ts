63cf1e5de563f80a6bf8dd5f31694dd83848d5b0
Madhumitha Subramaniyan <madhumitha.subramaniyan@hallmarkhcs.com>
2023-02-27 17:22:12 +0530

    task_ein_5623_agency_position_widget

---
+++ src/app/dashboard/widgets/agencyposition-widget/agencyposition-widget.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-agencyposition-widget',
+  templateUrl: './agencyposition-widget.component.html',
+  styleUrls: ['./agencyposition-widget.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class AgencypositionWidgetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
