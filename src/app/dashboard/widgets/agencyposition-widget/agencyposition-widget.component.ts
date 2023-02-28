1549413c2516e5b34a1f514d60a8ce2e65212dca
Madhumitha Subramaniyan <madhumitha.subramaniyan@hallmarkhcs.com>
2023-02-28 21:57:41 +0530

    task_ein_5263_widget

--- src/app/dashboard/widgets/agencyposition-widget/agencyposition-widget.component.ts
+++ src/app/dashboard/widgets/agencyposition-widget/agencyposition-widget.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, Inject, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Actions } from '@ngxs/store';
+import { DashboardService } from '../../services/dashboard.service';
+import { GlobalWindow } from '@core/tokens';
+import {  AgencyPositionModel } from '../../models/agency-position.model';
 
 @Component({
   selector: 'app-agencyposition-widget',
@@ -7,10 +11,28 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AgencypositionWidgetComponent implements OnInit {
+  @Input() public isLoading: boolean;
+  @Input() public isDarkTheme: boolean | false;
+  @Input() public description: string;
+  @Input() public agencyData: AgencyPositionModel | undefined;
+  public agencydata:any;
+  private mousePosition = {
+    x: 0,
+    y: 0,
+  };
 
-  constructor() { }
+  constructor(private readonly dashboardService: DashboardService,private actions$: Actions, private cdr: ChangeDetectorRef,
+              @Inject(GlobalWindow) protected readonly globalWindow : WindowProxy & typeof globalThis) {}
 
   ngOnInit(): void {
+     this.agencydata = this.agencyData;
   }
-
+  
+  public defineMousePosition($event: MouseEvent): void {
+    this.mousePosition.x = $event.screenX;
+    this.mousePosition.y = $event.screenY;
+  }
+  public toSourceContent(orgId:number): void {
+    this.dashboardService.redirectToUrl('client/order-management/',undefined);
+  }
 }
