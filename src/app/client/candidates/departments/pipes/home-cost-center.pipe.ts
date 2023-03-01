bda93a880dd5b8183c78316ef1af73f58a75895a
Roman Bishko <bishko86@yahoo.com>
2023-03-01 11:16:22 +0200

    put home cost center on the top of grid rows if it exists

---
+++ src/app/client/candidates/departments/pipes/home-cost-center.pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { DepartmentAssigned } from '../departments.model';
+
+@Pipe({
+  name: 'sortByHomeCostCenter',
+})
+export class HomeCostCenterPipe implements PipeTransform {
+  transform(value: DepartmentAssigned[]): DepartmentAssigned[] {
+    const isHomeCostCenter = value.findIndex((item) => item.isHomeCostCenter);
+    if (isHomeCostCenter >= 0) {
+      const [homeCostCenter] = value.splice(isHomeCostCenter, 1);
+      return [homeCostCenter, ...value];
+    } else {
+      return value;
+    }
+  }
+}
