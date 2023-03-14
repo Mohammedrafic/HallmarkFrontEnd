96047e816eef38794afb6d1a5e281295c535ac14
Yevhen Velychko <eveli@softserveinc.com>
2023-03-14 11:33:19 +0000

    Merged PR 30996: EIN-12805-settings-submission
    bug-ein-12805-settings fix submission logic

--- src/app/organization-management/settings/settings.component.ts
+++ src/app/organization-management/settings/settings.component.ts
@@ -766,6 +766,24 @@ export class SettingsComponent extends AbstractPermissionGrid implements OnInit,
       invoiceGneration.parsedValue = JSON.parse(invoiceGneration.value);
     }
 
+    const switchedValues: any[] = data.filter((setting: any) => {
+      return setting.controlType === this.organizationSettingControlType.SwitchedValue;
+    });
+
+    switchedValues.forEach((setting) => {
+      if (setting.value && typeof setting.value === 'string') {
+        setting.parsedValue = JSON.parse(setting.value);
+      }
+
+      if (setting.children && setting.children.length) {
+        setting.children.forEach((child: any) => {
+          if (child.value && typeof child.value === 'string') {
+            child.parsedValue = JSON.parse(child.value);
+          }
+        });
+      }
+    });
+
     return data.slice(
       currentPage * this.getActiveRowsPerPage() - this.getActiveRowsPerPage(),
       currentPage * this.getActiveRowsPerPage()
