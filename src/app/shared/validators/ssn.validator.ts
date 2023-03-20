14dbd1067572b2be28d169a6935e4a7481cdad46
Sridhar Jonnalagadda <sridhar.jonnalagadda@hallmarkhcs.com>
2023-03-20 09:38:18 -0400

    SSN Validation

--- src/app/shared/validators/ssn.validator.ts
+++ src/app/shared/validators/ssn.validator.ts
@@ -3,7 +3,7 @@ import { AbstractControl, ValidatorFn } from '@angular/forms';
 export function ssnValidator(): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} | null => {
     const ssnRegex = /^(?!219099999|078051120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4}$/;
-    debugger;
+
     if (control.value && !ssnRegex.test(control.value)) {
       return { ssn: true };
     }
