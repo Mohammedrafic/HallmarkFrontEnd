4eb951a1167380f51bd9c8de665f6af71ca13663
Roman Bishko <bishko86@yahoo.com>
2023-04-10 16:35:45 +0300

    sort imports

--- src/app/client/candidates/candidate-work-commitment/candidate-work-commitment.module.ts
+++ src/app/client/candidates/candidate-work-commitment/candidate-work-commitment.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { FeatherModule } from 'angular-feather';
+import { ButtonModule, SwitchModule } from '@syncfusion/ej2-angular-buttons';
+import { DateTimePickerModule } from '@syncfusion/ej2-angular-calendars';
+import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
+
+import { GridModule } from '@shared/components/grid/grid.module';
+import { SharedModule } from '@shared/shared.module';
 import { InputModule } from '@shared/components/form-controls/input/input.module';
 import { DatepickerModule } from '@shared/components/form-controls/datepicker/datepicker.module';
 import { DropdownModule } from '@shared/components/form-controls/dropdown/dropdown.module';
 import { MultiselectDropdownModule } from
   '@shared/components/form-controls/multiselect-dropdown/multiselect-dropdown.module';
-import { ButtonModule, SwitchModule } from '@syncfusion/ej2-angular-buttons';
 import { TextareaModule } from '@shared/components/form-controls/textarea/textarea.module';
 import { NumericTextboxModule } from '@shared/components/form-controls/numeric-textbox/numeric-textbox.module';
-import { GridModule } from '@shared/components/grid/grid.module';
-import { FeatherModule } from 'angular-feather';
-import { SharedModule } from '@shared/shared.module';
-import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
 import { CandidateWorkCommitmentComponent } from './candidate-work-commitment.component';
 import { CandidateWorkCommitmentGridComponent } from
   './components/candidate-work-commitment-grid/candidate-work-commitment-grid.component';
@@ -20,9 +24,12 @@ import { CandidateCommitmentGridActionRendererComponent } from
   './components/candidate-work-commitment-grid/grid-action-renderer/grid-action-renderer.component';
 import { CandidateWorkCommitmentDialogComponent } from
   './components/candidate-work-commitment-dialog/candidate-work-commitment-dialog.component';
-import { DialogModule } from '@syncfusion/ej2-angular-popups';
 import { CandidateWorkCommitmentService } from './services/candidate-work-commitment.service';
 import { AvailabilityRestrictionComponent } from './components/availability-restriction/availability-restriction.component';
+import { AvailabilityRestrictionDialogComponent } from
+  './components/availability-restriction-dialog/availability-restriction-dialog.component';
+import { AvailabilityApiService } from './services/availability-api.service';
+import { AvailabilityHelperService } from './services/availability-helper.service';
 
 @NgModule({
   declarations: [
@@ -31,12 +38,14 @@ import { AvailabilityRestrictionComponent } from './components/availability-rest
     CandidateCommitmentGridActionRendererComponent,
     CandidateWorkCommitmentDialogComponent,
     AvailabilityRestrictionComponent,
+    AvailabilityRestrictionDialogComponent,
   ],
   exports: [],
   imports: [
     CommonModule,
     InputModule,
     DatepickerModule,
+    DateTimePickerModule,
     DropdownModule,
     MultiselectDropdownModule,
     SwitchModule,
@@ -52,6 +61,8 @@ import { AvailabilityRestrictionComponent } from './components/availability-rest
   ],
   providers: [
     CandidateWorkCommitmentService,
+    AvailabilityApiService,
+    AvailabilityHelperService,
   ],
 })
 export class CandidateWorkCommitmentModule { }
