7f0cb5318309a44d663d52a77bb6a0d427ecb0bd
Madhumitha Subramaniyan <madhumitha.subramaniyan@hallmarkhcs.com>
2023-04-17 16:38:45 +0530

    task_ein_13468_ordertypes

--- src/app/shared/enums/order-type.ts
+++ src/app/shared/enums/order-type.ts
@@ -25,7 +25,9 @@ export const OrderTypeTitlesMap = new Map([
 ]);
 
 export const OrderTypeOptions = [
+  { id: OrderType.ContractToPerm, name: 'Contract To Perm' },
   { id: OrderType.OpenPerDiem, name: 'Open Per Diem' },
+  { id: OrderType.PermPlacement, name: 'Perm. Placement' },
   { id: OrderType.Traveler, name: 'Traveler' }
 ];
 
