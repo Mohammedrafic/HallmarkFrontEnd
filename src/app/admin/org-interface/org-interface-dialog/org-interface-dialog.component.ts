9dc2a52cb4a20d97a9e2fa8644b33e7c33c65d5c
Amarendra Gottumukkala <amarendra.gottumukkala@hallmarkhcs.com>
2023-04-25 12:37:15 +0530

    [EIN-14493] - Org interface configuration mappings

--- src/app/admin/org-interface/org-interface-dialog/org-interface-dialog.component.ts
+++ src/app/admin/org-interface/org-interface-dialog/org-interface-dialog.component.ts
@@ -1,8 +1,16 @@
+import { ColDef, GridApi, GridOptions, GridReadyEvent } from '@ag-grid-community/core';
 import { Component, OnInit, ChangeDetectionStrategy, Input, ViewChild, OnDestroy } from '@angular/core';
+import { Select } from '@ngxs/store';
+import { SpecialProjectMessages } from '@organization-management/specialproject/constants/specialprojects.constant';
+import { AbstractGridConfigurationComponent } from '@shared/components/abstract-grid-configuration/abstract-grid-configuration.component';
+import { ColumnDefinitionModel } from '@shared/components/grid/models';
+import { CustomNoRowsOverlayComponent } from '@shared/components/overlay/custom-no-rows-overlay/custom-no-rows-overlay.component';
+import { GRID_CONFIG } from '@shared/constants';
 import { OrgInterface } from '@shared/models/org-interface.model';
 import { disabledBodyOverflow, windowScrollTop } from '@shared/utils/styles.utils';
 import { DialogComponent } from '@syncfusion/ej2-angular-popups';
-import { Subject, takeUntil, takeWhile } from 'rxjs';
+import { Observable, Subject, takeUntil, takeWhile } from 'rxjs';
+import { AppState } from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-org-interface-dialog',
@@ -10,12 +18,15 @@ import { Subject, takeUntil, takeWhile } from 'rxjs';
   styleUrls: ['./org-interface-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class OrgInterfaceDialogComponent implements OnInit,  OnDestroy {
+export class OrgInterfaceDialogComponent extends AbstractGridConfigurationComponent implements OnInit,  OnDestroy {
 
   @Input() selectedOrgLog$: Subject<OrgInterface> = new Subject<OrgInterface>();
   @Input() openDialogue: Subject<boolean>;
 
   @ViewChild('sideOrgInterfaceDialog') sideOrgInterfaceDialog: DialogComponent;
+
+  @Select(AppState.isDarkTheme)
+  isDarkTheme$: Observable<boolean>;
 
   private isAlive = true;
   selectedOrgLogDataset:OrgInterface;
@@ -25,7 +36,83 @@ export class OrgInterfaceDialogComponent implements OnInit,  OnDestroy {
   onClearEvent$: Subject<boolean> = new Subject<boolean>();
   public targetElement: HTMLElement | null = document.body.querySelector('#main');
 
-  constructor() { }
+  public rowSelection: 'single' | 'multiple' = 'multiple';
+  public readonly gridConfig: typeof GRID_CONFIG = GRID_CONFIG;
+
+  public commonColumn: ColDef = {
+    sortable: true,
+    resizable: true,
+    filter: true,
+    editable: true,
+  }
+
+  public gridApi!: GridApi;
+  public rowData: any[] = [];
+
+  public columnDefinitions: any[] = 
+  [
+    /*{
+      field: '',
+      headerName: 'Action',
+      sortable: false,
+      resizable: false,
+      filter: false,
+    },*/
+    {
+      field: 'id',
+      headerName: 'id',
+      hide: true,
+      sortable: true,
+      resizable: false,
+      filter: false,
+    },
+    {
+      field: 'name',
+      headerName: 'name',
+      ...this.commonColumn,
+   //   cellRenderer: StatusTextCellrenderComponent
+    },
+   
+    {
+      field: 'fileName',
+      headerName: 'fileName',
+      ...this.commonColumn,
+    },
+    {
+      field: 'folderId',
+      headerName: 'folderId',
+      ...this.commonColumn
+    },
+    {
+      field: 'folderName',
+      headerName: 'folderName',
+      ...this.commonColumn,
+    },
+    {
+      field: 'docType',
+      headerName: 'docType',
+      ...this.commonColumn,
+    },
+    {
+      field: 'docTypeName',
+      headerName: 'docTypeName',
+      ...this.commonColumn,
+    },
+    {
+      field: 'tags',
+      headerName: 'tags',
+      ...this.commonColumn,
+    },
+    {
+      field: 'comments',
+      headerName: 'comments',
+      ...this.commonColumn,
+    },
+  ]
+
+  constructor() { 
+    super();
+  }
 
   ngOnInit(): void {
 
@@ -48,7 +135,95 @@ export class OrgInterfaceDialogComponent implements OnInit,  OnDestroy {
   public onClose(): void {
     this.sideOrgInterfaceDialog.hide();
     this.openDialogue.next(false);
+    this.gridApi.setRowData(this.rowData);
   }
+
+  onPageSizeChanged(event: any) {
+    this.gridOptions.cacheBlockSize = Number(event.value.toLowerCase().replace("rows", ""));
+    this.gridOptions.paginationPageSize = Number(event.value.toLowerCase().replace("rows", ""));
+    if (this.gridApi != null) {
+      this.gridApi.paginationSetPageSize(Number(event.value.toLowerCase().replace("rows", "")));
+      this.gridApi.setRowData(this.rowData);
+    }
+  }
+
+  public noRowsOverlayComponentParams: any = {
+    noRowsMessageFunc: () => SpecialProjectMessages.NoRowsMessage,
+  };
+
+  public sideBar = {
+    toolPanels: [
+      {
+        id: 'columns',
+        labelDefault: 'Columns',
+        labelKey: 'columns',
+        iconKey: 'columns',
+        toolPanel: 'agColumnsToolPanel',
+        toolPanelParams: {
+          suppressRowGroups: true,
+          suppressValues: true,
+          suppressPivots: true,
+          suppressPivotMode: true,
+          suppressColumnFilter: true,
+          suppressColumnSelectAll: true,
+          suppressColumnExpandAll: true,
+        },
+      },
+      {
+        id: 'filters',
+        labelDefault: 'Filters',
+        labelKey: 'filters',
+        iconKey: 'filters',
+        toolPanel: 'agFiltersToolPanel',
+        toolPanelParams: {
+          suppressRowGroups: true,
+          suppressValues: true,
+          suppressPivots: true,
+          suppressPivotMode: true,
+          suppressColumnFilter: true,
+          suppressColumnSelectAll: true,
+          suppressColumnExpandAll: true,
+        },
+      },
+    ],
+  };
+
+  gridOptions: GridOptions = {
+    pagination: true,
+    cacheBlockSize: this.pageSize,
+    paginationPageSize: this.pageSize,
+    columnDefs: this.columnDefinitions,
+    rowData: this.rowData,
+    sideBar: this.sideBar,
+    rowSelection: this.rowSelection,
+    noRowsOverlayComponent: CustomNoRowsOverlayComponent,
+    noRowsOverlayComponentParams: this.noRowsOverlayComponentParams,
+  /*  onFilterChanged: (event: FilterChangedEvent) => {
+      if (!event.api.getDisplayedRowCount()) {
+        this.gridApi?.showNoRowsOverlay();
+      }
+      else {
+        this.gridApi?.hideOverlay();
+      }
+    },*/
+    suppressRowClickSelection: true,
+  /*  onCellClicked: (e: CellClickedEvent) => {
+      const column: any = e.column;
+      if (column?.colId == documentsColumnField.Name) {
+        this.documentPreview(e.data);
+      }
+    }*/
+  };
+
+
+  
+
+  
+  onGridReady(params: GridReadyEvent) {
+    this.gridApi = params.api;
+    this.gridApi.setRowData(this.rowData);
+  }
+
 
   ngOnDestroy(): void {
     this.isAlive = false;
