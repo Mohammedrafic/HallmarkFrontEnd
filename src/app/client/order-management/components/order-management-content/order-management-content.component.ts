8d9f5a541dcf267ca9e3e12b7245200cd9a2af6c
Amarendra Gottumukkala <amarendra.gottumukkala@hallmarkhcs.com>
2023-04-25 17:59:55 +0530

    [EIN-14591] -Dashboard - Org specific My Tasks

--- src/app/client/order-management/components/order-management-content/order-management-content.component.ts
+++ src/app/client/order-management/components/order-management-content/order-management-content.component.ts
@@ -1861,19 +1861,22 @@ export class OrderManagementContentComponent extends AbstractPermissionGrid impl
 
     const dashboardFilterState = this.globalWindow.localStorage.getItem('dashboardFilterState') || 'null';
     const items = JSON.parse(dashboardFilterState) as FilteredItem[];
-    const filteredItems = items.filter((item: FilteredItem) =>
-      (item.organizationId === this.organizationId && item.column !== FilterColumnTypeEnum.ORGANIZATION)
-      || item.column === FilterColumnTypeEnum.SKILL
-    );
 
-    filteredItems.forEach((item: FilteredItem) => {
-      const filterKey = item.column as keyof OrderFilter;
-      if (filterKey in filters) {
-        (filters[filterKey] as number[]).push(item.value);
-      } else {
-        (filters[filterKey] as number[]) = [item.value];
-      }
-    });
+    if(items!= null && items.length >0){
+        const filteredItems = items.filter((item: FilteredItem) =>
+        (item.organizationId === this.organizationId && item.column !== FilterColumnTypeEnum.ORGANIZATION)
+        || item.column === FilterColumnTypeEnum.SKILL);
+
+        
+        filteredItems.forEach((item: FilteredItem) => {
+          const filterKey = item.column as keyof OrderFilter;
+          if (filterKey in filters) {
+            (filters[filterKey] as number[]).push(item.value);
+          } else {
+            (filters[filterKey] as number[]) = [item.value];
+          }
+        });
+    }
 
     this.orderPositionStatus = null;
     this.isRedirectedFromDashboard = false;
