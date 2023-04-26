65b6afef8b52661a1b170ef856803eb0346c9215
Mohammed Salman <mohammed@hallmarkhcs.com>
2023-04-27 01:21:29 +0530

    EIN-15045 Fix: Organization login/ User type drop down no "Employees"

--- src/app/admin/alerts/group-email/send-group-email/send-group-email.component.ts
+++ src/app/admin/alerts/group-email/send-group-email/send-group-email.component.ts
@@ -556,8 +556,8 @@ export class SendGroupEmailComponent
           } else {
             this.store.dispatch(new GetBusinessByUnitType(value));
             this.businessData$.pipe(takeWhile(() => this.isAlive)).subscribe((data) => {
+              this.businessData = data;
               if (!this.isBusinessFormDisabled && data.length > 0) {                
-                this.businessData = data;
                 if (this.groupEmailTemplateForm.controls['business'].value != data[0].id) {
                   this.groupEmailTemplateForm.controls['business'].setValue(data[0].id);
                   this.CheckBusinessIRPEnabled(data[0].id);
@@ -682,10 +682,12 @@ export class SendGroupEmailComponent
         this.locationsData = this.locationsList;
         this.departmentsData = this.departmentsList;        
         
-        this.CheckBusinessIRPEnabled(value);
-        this.filteredUserType = this.userType.filter((i: any) => i.isAgency == false);
-        if(!this.isCurrentBusinessHasIRPEnabled && this.businessUnitControl.value == 3){          
-          this.filteredUserType.pop();
+        if(value > 0 && this.businessUnitControl.value == 3){
+          this.CheckBusinessIRPEnabled(value);
+          this.filteredUserType = this.userType.filter((i: any) => i.isAgency == false);
+          if(!this.isCurrentBusinessHasIRPEnabled){          
+            this.filteredUserType.pop();
+          }
         }
         this.changeDetectorRef.detectChanges();        
       }
