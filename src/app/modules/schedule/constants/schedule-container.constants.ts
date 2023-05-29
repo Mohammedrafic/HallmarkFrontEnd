26157bc84a7cbad3a751c50b0af0e6121d04130d
Volodymyr Babii <vbabi@softserveinc.com>
2023-05-29 13:47:40 +0000

    Merged PR 35882: Adjust Responsiveness for Scheduling
    Adjust Responsiveness for Scheduling

--- src/app/modules/schedule/constants/schedule-container.constants.ts
+++ src/app/modules/schedule/constants/schedule-container.constants.ts
@@ -1,16 +1,3 @@
-import { TabsListConfig } from '@shared/components/tabs-list/tabs-list-config.model';
-
-import { TabTitleConfig } from '../enums';
-
-export const TabListConfig: TabsListConfig[] = [
-  {
-    title: TabTitleConfig.Scheduling,
-  },
-  // {
-  //   title: TabTitleConfig.Analytics,
-  // },
-];
-
 export const FilterErrorMessage = 'Filter to a single department and skill to start scheduling';
 
 export const HourTimeMealMs = 3600000;
