4ddf9f05432ffa19147ba6b7f162159f9af545f6
Volodymyr Babii <vbabi@softserveinc.com>
2023-06-08 12:23:24 +0000

    Merged PR 36570: Credential Group setup

--- src/app/shared/models/skill-group.model.ts
+++ src/app/shared/models/skill-group.model.ts
@@ -13,12 +13,16 @@ export class CredentialSkillGroupPost {
   id?: number;
   name: string;
   skillIds: number[];
+  includeInIRP? : boolean;
+  includeInVMS?: boolean;
 }
 
 export class CredentialSkillByOrganization {
   id: number;
   masterSkillId: number;
   name: string;
+  assignedToVMS: boolean;
+  assignedToIRP: boolean;
 }
 
 export type CredentialSkillGroupPage = PageOfCollections<CredentialSkillGroup>;
