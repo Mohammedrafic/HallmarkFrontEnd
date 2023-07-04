b24e551bde72e55e3024b02ef4f57f79effdd32d
Roman Bishko <rbish@softserveinc.com>
2023-07-04 09:38:26 +0300

    code formate

--- src/app/core/helpers/date-time.helper.spec.ts
+++ src/app/core/helpers/date-time.helper.spec.ts
@@ -425,7 +425,13 @@ describe('DateTimeHelper', () => {
   });
 
   describe('DateTimeHelper.getEarliestDate', () => {
-    const input = ['2023-06-01T00:00:00+00:00', '2024-06-09T00:00:00+00:00', '2023-07-01T00:00:00+00:00', '2023-06-01T16:22:00+00:00', '2023-06-14T00:00:00+00:00'];
+    const input = [
+      '2023-06-01T00:00:00+00:00',
+      '2024-06-09T00:00:00+00:00',
+      '2023-07-01T00:00:00+00:00',
+      '2023-06-01T16:22:00+00:00',
+      '2023-06-14T00:00:00+00:00',
+    ];
 
     it('should return the erliest date from string array', () => {
       const result = DateTimeHelper.getEarliestDate(input);
@@ -439,7 +445,13 @@ describe('DateTimeHelper', () => {
   });
 
   describe('DateTimeHelper.getLatestDate', () => {
-    const input = ['2024-06-09T18:22:00+00:00', '2023-09-01T00:00:00+00:00', '2023-06-05T16:22:00+00:00', '2022-06-11T00:00:00+00:00', '2023-06-01T00:00:00+00:00'];
+    const input = [
+      '2024-06-09T18:22:00+00:00',
+      '2023-09-01T00:00:00+00:00',
+      '2023-06-05T16:22:00+00:00',
+      '2022-06-11T00:00:00+00:00',
+      '2023-06-01T00:00:00+00:00',
+    ];
 
     it('should return the latest date from string array', () => {
       const result = DateTimeHelper.getLatestDate(input);
