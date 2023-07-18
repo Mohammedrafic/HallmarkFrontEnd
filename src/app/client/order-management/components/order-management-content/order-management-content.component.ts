478f2a36de947d6a0abc30ac939bce2ae70a3f36
Amarendra Gottumukkala <amarendra.gottumukkala@hallmarkhcs.com>
2023-07-18 15:07:35 +0530

    [EIN-19472]-page navigation fix

--- src/app/client/order-management/components/order-management-content/order-management-content.component.ts
+++ src/app/client/order-management/components/order-management-content/order-management-content.component.ts
@@ -1101,6 +1101,7 @@ export class OrderManagementContentComponent extends AbstractPermissionGrid impl
 
   public onFilterApply(): void {
     if (this.OrderFilterFormGroup.dirty) {
+      this.currentPage = 1;
       this.refreshFilterState();
       this.saveFiltersByPageName();
       this.getOrders(true);
