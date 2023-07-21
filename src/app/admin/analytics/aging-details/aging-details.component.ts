dab20b109b3bcfc14a69d39534c5ee12278cf19b
Aravinth Vijayakumar <aravinth.vijayakumar@hallmarkhcs.com>
2023-07-21 19:17:43 +0530

    Old code updated in settimeout

--- src/app/admin/analytics/aging-details/aging-details.component.ts
+++ src/app/admin/analytics/aging-details/aging-details.component.ts
@@ -206,7 +206,10 @@ export class AgingDetailsComponent implements OnInit, OnDestroy {
           this.defaultAgingGroups=this.agingGroups.map((list)=>list.id);
           this.agingReportForm.get(analyticsConstants.formControlNames.AgingGroupIds)?.setValue(this.defaultAgingGroups);
           if (this.isInitialLoad) {
+                  // setTimeout(() => {
               this.SearchReport();
+            // }, 0);
+
             this.isInitialLoad = false;
           }
           this.changeDetectorRef.detectChanges();
@@ -396,9 +399,5 @@ export class AgingDetailsComponent implements OnInit, OnDestroy {
     this.store.dispatch(new ShowToast(MessageTypes.Error, error));
     return;
   }
-  ngAfterViewInit(){
-    this.logiReportComponent.paramsData = this.paramsData;
-    this.logiReportComponent.RenderReport();
-  
-  }
+
 }
