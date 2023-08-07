e6313a10699c7bef03b0f05e717dbdaca3f92ba4
Aravinth Vijayakumar <aravinth.vijayakumar@hallmarkhcs.com>
2023-08-07 20:43:13 +0530

    End date calculation issue updated

--- src/app/shared/components/order-candidate-list/order-candidates-list/accept-candidate/accept-candidate.component.ts
+++ src/app/shared/components/order-candidate-list/order-candidates-list/accept-candidate/accept-candidate.component.ts
@@ -360,7 +360,7 @@ export class AcceptCandidateComponent implements OnInit, OnDestroy, OnChanges {
   private updateAgencyCandidateJob(applicantStatus: ApplicantStatus): void {
     console.log(applicantStatus)
     const value = this.form.getRawValue();
-    const jobStartDate = new Date(this.candidateJob.order.jobStartDate); 
+    const jobStartDate = new Date(this.candidateJob?.offeredStartDate); 
     const jobEndDate = new Date(this.candidateJob.order.jobEndDate);
     const daysDifference =  DateTimeHelper.getDateDiffInDays(jobStartDate, jobEndDate);
     const actualEndDate = this.calculateActualEndDate(jobStartDate, daysDifference).toISOString(); 
