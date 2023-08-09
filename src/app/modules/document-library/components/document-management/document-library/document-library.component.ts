d8b8bd13c5d336e77754485af04a237732313a68
Amarendra Gottumukkala <amarendra.gottumukkala@hallmarkhcs.com>
2023-08-09 16:04:01 +0530

    [EIN-17775]-removing the shared org or agency from the list

--- src/app/modules/document-library/components/document-management/document-library/document-library.component.ts
+++ src/app/modules/document-library/components/document-management/document-library/document-library.component.ts
@@ -375,6 +375,12 @@ export class DocumentLibraryComponent extends AbstractGridConfigurationComponent
                     this.documentLibraryform.get(FormControlNames.AllOrgnizations)?.setValue(true);
                 }
               }else{
+                if(this.agencySwitch) {
+                  this.AssociateAgencyData = this.AssociateAgencyData.filter(item1 => !data.some(item2 => (item2.id === item1.agencyId && item2.name === item1.agencyName)));
+                }
+                if(this.organizationSwitch) {
+                  this.ShareOrganizationsData = this.ShareOrganizationsData.filter(item1 => !data.some(item2 => (item2.id === item1.id && item2.name === item1.name)));
+                }
                 this.sharedWith?.gridOptions?.api?.setRowData(data);
               }
             }
