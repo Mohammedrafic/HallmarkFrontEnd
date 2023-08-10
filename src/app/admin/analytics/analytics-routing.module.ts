5c6c7c939c53cdd6b8385e7585570e99fec03d8c
Prasath Chinnasamy <prasath.chinnasamy@hallmarkhcs.com>
2023-08-10 15:21:37 +0530

    EIN-19193 Front end Screen for Department spend and hours report

--- src/app/admin/analytics/analytics-routing.module.ts
+++ src/app/admin/analytics/analytics-routing.module.ts
@@ -54,6 +54,7 @@ import { VmsInvoiceReportBetaComponent } from './vms-invoice-report-beta/vms-inv
 import { PositionSummaryComponent } from './Position-Summary/Position-Summary.component';
 import { ShiftBreakdownComponent } from './shift-breakdown/shift-breakdown.component';
 import { FinancialTimeSheetBetaComponent } from './financial-time-sheet-beta/financial-time-sheet-beta.component';
+import { DepartmentSpendAndHoursComponent } from './department-spend-and-hours/department-spend-and-hours.component';
 
 const routes: Routes = [
   {
@@ -420,6 +421,14 @@ const routes: Routes = [
       {
         path: 'financial-time-sheet-beta',
         component: FinancialTimeSheetBetaComponent,
+        data: {
+          isOrganizationArea: true,
+        }
+      }
+      ,
+      {
+        path: 'department-spend-and-hours-report',
+        component: DepartmentSpendAndHoursComponent,
         data: {
           isOrganizationArea: true,
         }
