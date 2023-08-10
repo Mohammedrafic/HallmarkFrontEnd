fcab85289a902665f69834dec35928fc1a49e8f4
Amarendra Gottumukkala <amarendra.gottumukkala@hallmarkhcs.com>
2023-08-10 17:24:01 +0530

    [EIN-17775]-validation check

--- src/app/admin/alerts/group-email/send-group-email/send-group-email.component.ts
+++ src/app/admin/alerts/group-email/send-group-email/send-group-email.component.ts
@@ -572,6 +572,7 @@ export class SendGroupEmailComponent
     this.businessUnitControl.valueChanges.pipe(distinctUntilChanged(), takeWhile(() => this.isAlive)).subscribe((value) => {
       this.onFormvalidation([]);
       this.groupEmailTemplateForm.markAsUntouched();
+      this.emailBodyRequired$.next(false);
       this.isBusinessUnitTypeAgency = false;
       this.validationCheckForBusiness();
       if (this.isSend == true && value != null) {
