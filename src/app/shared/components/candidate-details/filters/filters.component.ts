04600e08f5d1292892000283ceafcd3d3c274efb
PauthiraPerumal <PauthiraPerumal@HHS-LAP-384>
2023-08-24 19:10:08 +0530

    Ein-21214,Ein-21219,Ein-21082 candidate Assignment

--- src/app/shared/components/candidate-details/filters/filters.component.ts
+++ src/app/shared/components/candidate-details/filters/filters.component.ts
@@ -33,6 +33,7 @@ export class FiltersComponent extends DestroyableDirective implements OnInit, Af
     text: 'name',
     value: 'id',
   };
+  public maxlengh: Number = 10;
   public typeFields: FieldSettingsModel = { text: 'name', value: 'id' };
   public skillFields: FieldSettingsModel = { text: 'skillDescription', value: 'masterSkillId' };
   public orgid:number|null|undefined;
@@ -48,6 +49,7 @@ export class FiltersComponent extends DestroyableDirective implements OnInit, Af
     const user = this.store.selectSnapshot(UserState.user);
     this.orgid=user?.businessUnitId
   }
+  public htmlAttributes = {  maxlength: "50" };
   remoteWaterMark: string = 'e.g. Andrew Fuller';
   commonFields: FieldSettingsModel = { text: 'name', value: 'id' };
   candidateNameFields: FieldSettingsModel = { text: 'fullName', value: 'id' };
