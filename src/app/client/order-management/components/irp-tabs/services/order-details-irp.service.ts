ac235668ed93979e1b2e9575d73eeb96fc5493e7
Anbarasan Ramasubbu <anbarasan.ramasubbu@hallmarkhcs.com>
2023-09-05 17:47:05 +0530

    EIN-21878

--- src/app/client/order-management/components/irp-tabs/services/order-details-irp.service.ts
+++ src/app/client/order-management/components/irp-tabs/services/order-details-irp.service.ts
@@ -85,7 +85,7 @@ export class OrderDetailsIrpService {
       weekend: [false],
       holiday: [false],
       jobDescription: ['', Validators.maxLength(4000)],
-      unitDescription: ['', Validators.maxLength(1000)],
+      unitDescription: ['', Validators.maxLength(2000)],
     });
   }
 
