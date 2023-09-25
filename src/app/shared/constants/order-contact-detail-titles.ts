947b2d087b2b510054178a209910df7db0e00942
Sandeep Meka <sandeep.meka@hallmarkhcs.com>
2023-09-25 15:37:02 -0500

    Fixed the order of order contact detail titles.

--- src/app/shared/constants/order-contact-detail-titles.ts
+++ src/app/shared/constants/order-contact-detail-titles.ts
@@ -1,8 +1,8 @@
 export const ORDER_CONTACT_DETAIL_TITLES: string[] = [
+  'Director',
   'Manager',
   'Onboarding Coordinator',
   'Staffing Coordinator',
   'Supervisor',
   'Timesheet Approver',
-  'Director',
 ];
