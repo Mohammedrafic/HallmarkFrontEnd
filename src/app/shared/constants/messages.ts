e55c3b2ad27a6a9d697c4a98bbd1d1bf7c15aedc
Volodymyr Babii <vbabi@softserveinc.com>
2023-10-10 08:14:52 +0000

    Merged PR 44262: Incorrect warning text when editing workflow mapping
    Incorrect warning text when editing workflow mapping

--- src/app/shared/constants/messages.ts
+++ src/app/shared/constants/messages.ts
@@ -67,7 +67,7 @@ export const updateCandidateJobMessage = (dates: string[]) =>
 export const usedByOrderErrorMessage = (val: string, entities: string) =>
   `${val} cannot be deleted. This ${val} was used in ${entities}.`;
 export const usedInMappingMessage = (mappingName: string) =>
-  `This change has been affected ${mappingName} Mapping, please update it on the ${mappingName} Mapping tab`;
+  `This change has affected ${mappingName} Mapping. Please update it on the ${mappingName} Mapping tab.`;
 export const DEPLOYED_CANDIDATE = 'Deployed Candidate';
 export const deployedCandidateMessage = (orderIds: string[]) =>
   `<span class="deployed-candidate__message">Candidate is already working on order <span class="deployed-candidate__order-id"> ${orderIds.join(
