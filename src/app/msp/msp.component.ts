498a436d233aca26ca1086430bc4cdd32d87b5e7
Anbarasan Ramasubbu <anbarasan.ramasubbu@hallmarkhcs.com>
2023-10-24 23:48:49 +0530

    EIN-24022

--- src/app/msp/msp.component.ts
+++ src/app/msp/msp.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-msp',
-  template: '<router-outlet></router-outlet>',
-  styleUrls: ['./msp.component.scss'],
+  template: '<router-outlet></router-outlet>',  
 })
 export class MspComponent  {
   constructor() {
