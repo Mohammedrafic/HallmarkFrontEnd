c465445642b59be2d50f7a44432b515186506db0
Sandeep Meka <sandeep.meka@hallmarkhcs.com>
2023-10-24 08:57:57 -0400

    EIN-24020
    MSP Implementation - Changes on Agency List Page

--- src/app/agency/agency-list/agency-list.constants.ts
+++ src/app/agency/agency-list/agency-list.constants.ts
@@ -61,8 +61,8 @@ export const MSPMenuOptions = (
   mSPMenuOption: [
     { text: MSPMenuType[0], id: '0' },
     { text: MSPMenuType[1], id: '1', disabled: isMSP },
-    { text: MSPMenuType[2], id: '2', disabled: !isMSP },
-    { text: MSPMenuType[3], id: '3' },
+    { text: MSPMenuType[2], id: '2', disabled: true },//re add after implementation - disabled: !isMSP
+    { text: MSPMenuType[3], id: '3', disabled: true },// remove - disabled: true  after implementations.
   ]
 });
 
