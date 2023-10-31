e36c7e5cf95ccf627da76a672a55dc3c87e0a414
DeviMeyyappan <devi.meyyappan@hallmarkhcs.com>
2023-10-31 20:03:27 +0530

    EIN-7539 Feature Inactivating Shifts

--- src/app/organization-management/shifts/shifts.constants.ts
+++ src/app/organization-management/shifts/shifts.constants.ts
@@ -5,4 +5,8 @@ export const ShiftExportColumns: ExportColumn[] = [
   { text: 'Start Time', column: 'StartTime' },
   { text: 'End Time', column: 'EndTime' },
   { text: 'On Call Shift ', column: 'OnCallText' },
+  { text: 'InactiveDate ', column: 'InactiveDate' },
 ];
+export const InactivateColFormat = {
+  type:'date', format: 'MM/dd/yyyy',
+};
