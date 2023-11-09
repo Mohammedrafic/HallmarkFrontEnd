a0c54fafccd76c319b4b6d2ee128e85b06cdff3c
Vignesh Duraisamy <vignesh.duraisamy@hallmarkhcs.com>
2023-11-09 17:12:21 +0530

    EIN-24935

--- src/app/client/order-management/components/irp-tabs/order-details/validators/job-distribution.validator.ts
+++ src/app/client/order-management/components/irp-tabs/order-details/validators/job-distribution.validator.ts
@@ -13,10 +13,12 @@ export function jobDistributionValidator(controlName: string,distributeToVMS:str
   return (formGroup: AbstractControl): ValidationErrors | null => {
     const control = formGroup.get(controlName);
     const value = control?.value;
+console.log(value);
     const allInternalSelected = value && value?.includes(IrpOrderJobDistribution.AllInternal)
     && value?.includes(IrpOrderJobDistribution.TieringLogicInternal);
-    const internalNotSelected = !value?.includes(IrpOrderJobDistribution.AllInternal) && 
-    !value?.includes(IrpOrderJobDistribution.TieringLogicInternal);
+
+    const internalNotSelected = value && (!value?.includes(IrpOrderJobDistribution.AllInternal) &&
+    !value?.includes(IrpOrderJobDistribution.TieringLogicInternal));
 
     if (internalNotSelected && !isTemplate) {
       control?.setErrors({ errorMessage: InternalNotSelectedError });
