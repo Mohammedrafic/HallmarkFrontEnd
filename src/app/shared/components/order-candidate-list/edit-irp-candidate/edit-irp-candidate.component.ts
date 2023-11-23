3de0b3344ae9af78db3572e09fe427b67898cf95
Madhumitha Subramaniyan <madhumitha.subramaniyan@hallmarkhcs.com>
2023-11-23 21:26:20 +0530

    EIN-25707 ATPStipend rate calling multiple times fixed

--- src/app/shared/components/order-candidate-list/edit-irp-candidate/edit-irp-candidate.component.ts
+++ src/app/shared/components/order-candidate-list/edit-irp-candidate/edit-irp-candidate.component.ts
@@ -228,6 +228,7 @@ export class EditIrpCandidateComponent extends Destroyable implements OnInit {
       this.AtpCalcForm.get("hoursWorked")?.valueChanges.pipe(takeUntil(this.componentDestroy())).subscribe((data) => {
         if(data){
           this.hoursWorked = data;
+          console.log("atpstipendrate4");
           this.getATPstipendRate();
         }
       })
@@ -235,6 +236,7 @@ export class EditIrpCandidateComponent extends Destroyable implements OnInit {
       this.AtpCalcForm.get("costSaving")?.valueChanges.pipe(takeUntil(this.componentDestroy())).subscribe((data) => {
         if(data){
           this.costSaving = data;
+          console.log("atpstipendrate3");
           this.getATPstipendRate();
         }
       });
@@ -250,13 +252,17 @@ export class EditIrpCandidateComponent extends Destroyable implements OnInit {
           this.showbenefits = true;
           this.shownonbenefits = true;
         }
+        console.log("atpstipendrate2");
         this.getATPstipendRate();
       });
 
       this.candidateForm.get("actualStartDate")?.valueChanges.pipe(takeUntil(this.componentDestroy())).subscribe((data) => {
         if(data){
           this.candidateDetails.actualStartDate = data;
-          this.getATPstipendRate();
+          console.log("atpstipendrate1");
+          if(this.candidateForm.get('status')?.value === ApplicantStatus.OnBoarded){
+            this.getATPstipendRate();
+          }
         }
       });
     }
@@ -511,6 +517,7 @@ export class EditIrpCandidateComponent extends Destroyable implements OnInit {
         filter(Boolean),
         tap((candidateDetails: CandidateDetails) => {
           this.candidateDetails = candidateDetails;
+          console.log("atpstipendrate5");
           this.getATPstipendRate();
           this.updateVisibilityConfig(this.candidateModelState.candidate.status, candidateDetails);
           this.handleOfferedStatus(
@@ -660,6 +667,7 @@ export class EditIrpCandidateComponent extends Destroyable implements OnInit {
       distinctUntilChanged(),
       takeUntil(this.componentDestroy()),
     ).subscribe((status : string) => {
+      console.log(status);
       if(JSON.parse(status) == ApplicantStatus.OnBoarded){
         if(this.meal !== 0){
           this.showATPform = true;
