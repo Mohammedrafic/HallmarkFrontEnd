ad1988d59bdf193e4a21a9d18baa6166197af871
vkhyt <vkhyt@softserveinc.com>
2023-12-15 12:52:36 +0200

    EIN-27266 Adjust min date

--- src/app/organization-management/settings/settings.component.ts
+++ src/app/organization-management/settings/settings.component.ts
@@ -148,6 +148,7 @@ export class SettingsComponent extends AbstractPermissionGrid implements OnInit,
   payPeriodFormGroup: FormGroup;
   aTPRateCalculationFormGroup : FormGroup;
   startsOnFormGroup: FormGroup;
+  startsOnMinDate: Date | null;
 
   dropdownDataSource: OrganizationSettingValueOptions[];
   allRegions: OrganizationRegion[] = [];
@@ -826,9 +827,11 @@ export class SettingsComponent extends AbstractPermissionGrid implements OnInit,
       dynamicValue = SettingsDataAdapter.getParsedValue(valueOptions);
       const startsOn = dynamicValue.StartsOn || dynamicValue.startsOn;
       const isEnabled = dynamicValue.IsEnabled || dynamicValue.isEnabled;
+      const startsOnDate = startsOn ? DateTimeHelper.setCurrentTimeZone(startsOn) : null;
 
+      this.startsOnMinDate = startsOnDate;
       this.startsOnFormGroup.setValue({
-        startsOn: startsOn ? new Date(startsOn) : new Date(),
+        startsOn: startsOnDate,
         isEnabled: isEnabled ? isEnabled : false,
       });
     }
