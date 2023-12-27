7bb419b8e5232c943a4710f714a6808a6db979fe
Brandon Maharaj <brandon.maharaj@hallmarkhcs.com>
2023-12-27 13:25:03 -0500

    feat(login-page): Update login page component

--- src/app/b2c-auth/login-page/login-page.component.ts
+++ src/app/b2c-auth/login-page/login-page.component.ts
@@ -2,14 +2,13 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Store } from '@ngxs/store';
-import { filter, takeUntil, tap } from 'rxjs';
-import { createSpinner, showSpinner } from '@syncfusion/ej2-angular-popups';
+import { takeUntil } from 'rxjs';
+import { createSpinner, hideSpinner, showSpinner } from '@syncfusion/ej2-angular-popups';
 
 import { DestroyableDirective } from '@shared/directives/destroyable.directive';
 import { B2CAuthService } from 'src/app/b2c-auth/b2c-auth.service';
 import { UserState } from 'src/app/store/user.state';
 import { SsoManagement } from '../sso-management';
-import { InteractionType } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-login-page',
@@ -19,9 +18,7 @@ import { InteractionType } from '@azure/msal-browser';
 export class LoginPageComponent extends DestroyableDirective implements AfterViewInit {
   @ViewChild('spiner') spiner: ElementRef;
 
-  constructor(private router: Router,
-              private b2CAuthService: B2CAuthService,
-              private store: Store) {
+  constructor(private router: Router, private b2CAuthService: B2CAuthService, private store: Store) {
     super();
   }
 
@@ -29,29 +26,40 @@ export class LoginPageComponent extends DestroyableDirective implements AfterVie
     createSpinner({
       target: this.spiner.nativeElement,
     });
-    // B2C Login
-    this.b2CAuthService.onLoginSuccess().pipe(
-      takeUntil(this.destroy$),
-      tap(() => showSpinner(this.spiner.nativeElement)),
-      filter((response) => (
-        response.interactionType === InteractionType.Redirect ||
-        response.interactionType === InteractionType.Popup)),
-      tap(() => this.navigateToDefaultPage())
-    ).subscribe();
 
-    this.b2CAuthService.interactionStatusNone().pipe(
-      takeUntil(this.destroy$),
-      filter(() => (!this.b2CAuthService.isLoggedIn())),
-      tap(() => this.loginWithSSO())
-    ).subscribe();
+    // B2C Login
+    this.b2CAuthService
+      .onLoginSuccess()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => this.navigateToDefaultPage());
+
+    this.b2CAuthService
+      .interactionStatusNone()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        if (!this.b2CAuthService.isLoggedIn()) {
+          this.loginWithSSO();
+        } else {
+          this.navigateToDefaultPage();
+        }
+      });
   }
 
   public loginWithSSO(): void {
-    showSpinner(this.spiner.nativeElement);
+    this.showSpinner();
     this.b2CAuthService.loginSSO();
   }
 
+  showSpinner(): void {
+    showSpinner(this.spiner?.nativeElement);
+  }
+
+  hideSpinner(): void {
+    hideSpinner(this.spiner?.nativeElement);
+  }
+
   private navigateToDefaultPage(): void {
+    this.hideSpinner();
     const isEmployee = this.store.selectSnapshot(UserState.user)?.isEmployee;
 
     if (isEmployee) {
@@ -69,4 +77,3 @@ export class LoginPageComponent extends DestroyableDirective implements AfterVie
     }
   }
 }
-
