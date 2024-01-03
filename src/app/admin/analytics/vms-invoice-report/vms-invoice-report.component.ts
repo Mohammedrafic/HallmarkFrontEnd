3574480e63df6500c2e0d99f0501277ab0d98537
Sandeep Reddy Mitta <sandeep.mitta@hallmarkhcs.com>
2024-01-03 12:02:15 +0530

    EIN-27662

--- src/app/admin/analytics/vms-invoice-report/vms-invoice-report.component.ts
+++ src/app/admin/analytics/vms-invoice-report/vms-invoice-report.component.ts
@@ -231,8 +231,69 @@ export class VmsInvoiceReportComponent implements OnInit, OnDestroy {
     this.unsubscribe$.complete();
   }
 
-  selectPeriod() {
+  selectPeriod(event: any) {
+    let { startDate, period } = this.vmsInvoiceReportForm.getRawValue();
+    const value = event.itemData.id;
     this.periodIsDefault = this.vmsInvoiceReportForm.controls['period'].value == "Custom" ? true : false;
+    this.vmsInvoiceReportForm.get(analyticsConstants.formControlNames.StartDate)?.setValue("");
+    this.vmsInvoiceReportForm.get(analyticsConstants.formControlNames.EndDate)?.setValue("");
+    const PeriodCheck = value;
+    let startDateControl = new Date(Date.now());
+    let endDateControl = new Date(Date.now());
+    let lastDayOfLastMonth = new Date();
+    lastDayOfLastMonth.setMonth(lastDayOfLastMonth.getMonth(), 0);
+
+    switch (PeriodCheck) {
+      case 0:
+        startDateControl.setDate(startDateControl.getDate() - 30);
+        break;
+      case 1:
+        startDateControl.setDate(startDateControl.getDate() - 31);
+        endDateControl.setDate(endDateControl.getDate() - 1);
+        break;
+      case 2:
+        startDateControl.setDate(startDateControl.getDate() - 61);
+        endDateControl.setDate(endDateControl.getDate() - 1);
+        break;
+      case 3:
+        startDateControl.setDate(startDateControl.getDate() - 91);
+        endDateControl.setDate(endDateControl.getDate() - 1);
+        break;
+      case 4:
+        startDateControl = new Date(startDateControl.getFullYear(), startDateControl.getMonth(), 1);
+        break;
+      case 5:
+        const today = new Date(Date.now());
+        const quarter = Math.floor((today.getMonth() / 3));
+        startDateControl = new Date(today.getFullYear(), quarter * 3 - 3, 1);
+        endDateControl = new Date(startDateControl.getFullYear(), startDateControl.getMonth() + 3, 0);
+        break;
+      case 6:
+        const startDate = new Date(startDateControl.getFullYear(), 0, 1)
+        startDate.setDate(startDate.getDate());
+        startDateControl = startDate;
+        break;
+      case 7:
+        const firstDay = new Date(startDateControl.getFullYear(), startDateControl.getMonth(), 1);
+        startDateControl = this.addMonths(firstDay, -6);
+        startDateControl.setDate(startDateControl.getDate());
+        endDateControl = new Date((lastDayOfLastMonth));
+        break;
+      case 8:
+        const dayFirst = new Date(startDateControl.getFullYear(), startDateControl.getMonth(), 1);
+        startDateControl = this.addMonths(dayFirst, -12);
+        startDateControl.setDate(startDateControl.getDate());
+        endDateControl = new Date((lastDayOfLastMonth));
+        break;
+    }
+
+    this.vmsInvoiceReportForm.get(analyticsConstants.formControlNames.StartDate)?.setValue(startDateControl);
+    this.vmsInvoiceReportForm.get(analyticsConstants.formControlNames.EndDate)?.setValue(new Date((endDateControl)));
+
+  }
+  private addMonths(date: any, months: any) {
+    date.setMonth(date.getMonth() + months);
+    return date;
   }
   private loadperiod(): void {
     this.periodList = [];
